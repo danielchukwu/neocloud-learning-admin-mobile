76c9dab267f56d2d1685fe450835173820be8413
dc <00danzy@gmail.com>
2023-03-31 21:34:06 +0100

    first commit

---
+++ lib/components/texts.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/constraints.dart';
+
+class TextSmall extends StatelessWidget {
+  TextSmall({
+    super.key,
+    required this.title,
+    this.color = Colors.black,
+    this.weight = FontWeight.w400,
+    this.textAlign = TextAlign.start
+  });
+
+  final TextAlign textAlign;
+  final String title;
+  final Color color;
+  final FontWeight weight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        color: color,
+        fontSize: defaultSize * 1.4,
+        fontWeight: weight,
+      ),
+    );
+  }
+}
+
+class TextMedium extends StatelessWidget {
+  const TextMedium({
+    super.key,
+    required this.title,
+    this.color = Colors.black,
+    this.weight = FontWeight.w400,
+    this.textAlign = TextAlign.start
+  });
+
+  final TextAlign textAlign;
+  final String title;
+  final Color color;
+  final FontWeight weight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        color: color,
+        fontSize: defaultSize * 1.6,
+        fontWeight: weight,
+      ),
+    );
+  }
+}
+
+class TextLarge extends StatelessWidget {
+  const TextLarge({
+    super.key,
+    required this.title,
+    this.color = Colors.black,
+    this.weight = FontWeight.w400,
+    this.textAlign = TextAlign.start
+  });
+
+  final TextAlign textAlign;
+  final String title;
+  final Color color;
+  final FontWeight weight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        color: color,
+        fontSize: defaultSize * 1.8,
+        fontWeight: weight,
+      ),
+    );
+  }
+}
+
+class TextExtraLarge extends StatelessWidget {
+  const TextExtraLarge({
+    super.key,
+    required this.title,
+    this.color = Colors.black,
+    this.weight = FontWeight.w400,
+    this.textAlign = TextAlign.start
+  });
+
+  final TextAlign textAlign;
+  final String title;
+  final Color color;
+  final FontWeight weight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        color: color,
+        fontSize: defaultSize * 2,
+        fontWeight: weight,
+      ),
+    );
+  }
+}
+
+class TextCustom extends StatelessWidget {
+  const TextCustom({
+    super.key,
+    required this.title,
+    required this.fontSize,
+    this.color = Colors.black,
+    this.weight = FontWeight.w400,
+    this.textAlign = TextAlign.start
+  });
+
+  final TextAlign textAlign;
+  final String title;
+  final double fontSize;
+  final Color color;
+  final FontWeight weight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        color: color,
+        fontSize: fontSize,
+        fontWeight: weight,
+
+      ),
+    );
+  }
+}
+
