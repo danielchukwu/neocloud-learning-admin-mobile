ae62ce229769035d51a7b661150f656bc1682885
dc <00danzy@gmail.com>
2023-04-01 18:21:25 +0100

    first commit

---
+++ lib/constraints.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/size_config.dart';
+
+// Neo Cloud Logo
+const neocloudLogo = 'assets/images/logo-dark.png';
+var defaultSize = SizeConfig.defaultSize!;
+
+Color kBlue = const Color(0xFF1679F7);
+Color kBlueLight = const Color(0xFF1679F7).withOpacity(.7);
+
+Color kBlack90 = const Color(0xFF000000).withOpacity(.9);
+Color kBlack80 = const Color(0xFF000000).withOpacity(.8);
+Color kBlack70 = const Color(0xFF000000).withOpacity(.7);
+Color kBlack60 = const Color(0xFF000000).withOpacity(.6);
+
+const kTextColor = Color(0xFF202E2E);
+const kTextLigntColor = Color(0xFF7286A5);
