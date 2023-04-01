c3d081fe5799c7cdd12067ca22713ac4c862749a
dc <00danzy@gmail.com>
2023-04-01 18:22:35 +0100

    add logo builder widget

--- lib/components/images.dart
+++ lib/components/images.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/constraints.dart';
 
 class Logo extends StatelessWidget {
-  Logo({
+  final String icon;
+  final double size;
+
+  const Logo({
     super.key,
     required this.icon,
+    required this.size,
   });
-  final String icon;
 
   @override
   Widget build(BuildContext context) {
     return Image(
       image: AssetImage(icon),
-      width: defaultSize * 27,
+      width: size,
     );
   }
 }
