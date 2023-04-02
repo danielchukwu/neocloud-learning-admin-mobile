a9ca4e837ee062343d8f39128056dd71c066fded
dc <00danzy@gmail.com>
2023-04-02 22:56:42 +0100

    first commit

---
+++ lib/screens/dashboard/components/circle_avatar_text.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/texts.dart';
+import 'package:neocloud_mobile/constraints.dart';
+
+class CircleAvatarAndText extends StatelessWidget {
+  const CircleAvatarAndText({
+    super.key,
+    required this.press,
+    this.image,
+    this.text = '',
+    this.width = 60,
+    this.height = 60,
+  });
+
+  final String? image;
+  final String text;
+  final double width;
+  final double height;
+  final Function(BuildContext context) press;
+
+  @override
+  Widget build(BuildContext context) {
+    String firstName = text!.split(' ')[0];
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        // Avatar
+        InkWell(
+          onTap: () => press(context),
+          child: image != null
+              ? RoundBoxAvatar(width: width, height: height, image: image)
+              : RoundBoxIcon(width: width, height: height, icon: Icons.add),
+        ),
+
+        // Text - Users First Name
+        SizedBox(height: defaultSize * .7),
+        TextSmall(title: firstName = text!.split(' ')[0], color: kBlack50),
+      ],
+    );
+  }
+}
+
+class RoundBoxAvatar extends StatelessWidget {
+  const RoundBoxAvatar({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.image,
+  });
+
+  final double width;
+  final double height;
+  final String? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      margin: EdgeInsets.symmetric(horizontal: defaultSize * .75),
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image!)),
+        color: kBlack.withOpacity(.05),
+        borderRadius: BorderRadius.circular(defaultSize * 5),
+      ),
+    );
+  }
+}
+
+class RoundBoxIcon extends StatelessWidget {
+  const RoundBoxIcon({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.icon,
+  });
+
+  final double width;
+  final double height;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      margin: EdgeInsets.symmetric(horizontal: defaultSize * .75),
+      decoration: BoxDecoration(
+        color: kBlack.withOpacity(.05),
+        borderRadius: BorderRadius.circular(defaultSize * 5),
+      ),
+      child: Icon(icon, color: kBlack50),
+    );
+  }
+}
