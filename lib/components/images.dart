1a5cfc2501a77a58f40189499b8153db61c15f87
dc <00danzy@gmail.com>
2023-04-02 22:57:09 +0100

    first commit

--- lib/components/images.dart
+++ lib/components/images.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/constraints.dart';
 
-class Logo extends StatelessWidget {
+class DisplayAssetImage extends StatelessWidget {
   final String icon;
   final double size;
 
-  const Logo({
+  const DisplayAssetImage({
     super.key,
     required this.icon,
     required this.size,
