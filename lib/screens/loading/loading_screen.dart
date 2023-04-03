228a6ad7c901df8d76e13795e3eb12da9ad6df24
dc <00danzy@gmail.com>
2023-04-03 20:56:24 +0100

    use DisplayAssetImage

--- lib/screens/loading/loading_screen.dart
+++ lib/screens/loading/loading_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/images.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/size_config.dart';
 
 class LoadingScreen extends StatelessWidget {
+  final String iconAsset = 'assets/images/logo-light-sm.png';
+
   const LoadingScreen({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +15,8 @@ class LoadingScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Container(
-            width: defaultSize! * 20,
-            margin: EdgeInsets.only(bottom: defaultSize * 0.3),
-            child: Image(
-              image: AssetImage('assets/images/logo-dark.png'),
-            ),
+            margin: EdgeInsets.only(bottom: defaultSize * 5),
+            child: DisplayAssetImage(icon: iconAsset, size: defaultSize * 4),
           )
         ),
       ),
