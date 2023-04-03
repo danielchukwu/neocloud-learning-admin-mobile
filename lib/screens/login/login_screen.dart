7bdb49cc425764e22abcdab0e7808c11ffee3d99
dc <00danzy@gmail.com>
2023-04-03 20:54:06 +0100

    improve structure with proper spacing

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/buttons.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/screens/login/components/appbar_back_button.dart';
+import 'package:neocloud_mobile/screens/login/components/appbar_leading_icon.dart';
 import 'package:neocloud_mobile/screens/login/components/intro_text.dart';
 import 'package:neocloud_mobile/screens/login/components/login_form.dart';
 
@@ -11,19 +12,20 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: defaultSize * 2),
+      body: SingleChildScrollView(
+        padding: screenPadding,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // Section 1 - Intro Text
-            const Spacer(flex: 3,),
+            SizedBox(height: defaultSize * 10),
             const IntroText(),
 
             // Section 2 - Form
-            const Spacer(flex: 1),
+            SizedBox(height: defaultSize),
             LoginForm(),
 
-            const Spacer(flex: 8,),
+            SizedBox(height: defaultSize * 10,)
           ],
         ),
       ),
@@ -33,7 +35,7 @@ class LoginScreen extends StatelessWidget {
   PreferredSize buildAppBar() {
     return PreferredSize(
       preferredSize: Size.fromHeight(defaultSize * 5.8),
-      child: const AppBarBackButton(),
+      child: AppBar(leading: const LoginAppBarBackButton()),
     );
   }
 }
