5a369a8ddcb6bad1c695ea9e3c34e1f888add0e5
dc <00danzy@gmail.com>
2023-04-07 21:36:04 +0100

    replace repeated image display code with a reusable

--- lib/components/cards.dart
+++ lib/components/cards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/images.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/models/Courses.dart';
@@ -14,58 +15,48 @@ class CourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 50),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // Course Image
-          Container(
-            margin: EdgeInsets.only(top: defaultSize * 2),
-            height: defaultSize * 20,
-            width: SizeConfig.screenWidth! - appsBodyPadding,
-            decoration: BoxDecoration(
-                color: kBlack.withOpacity(.2),
-                image: DecorationImage(
-                  image: AssetImage(course.imgSrc),
-                  fit: BoxFit.cover,
-                )),
-          ),
-          SizedBox(height: defaultSize * 2),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // Course Image
+        SizedBox(height: defaultSize * 3),
+        RectangularBoxImage(img: course.imgSrc, height: defaultSize * 20),
 
-          // Title
-          TextCustom(
-            title: course.title,
-            fontSize: defaultSize * 2.2,
-            color: kBlack80,
-            weight: FontWeight.w700,
-          ),
-          SizedBox(height: defaultSize),
+        // Title
+        SizedBox(height: defaultSize * 2),
+        TextCustom(
+          title: course.title,
+          fontSize: defaultSize * 2.2,
+          color: kBlack80,
+          weight: FontWeight.w700,
+        ),
 
-          // Name and Ratings
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextMedium(title: course.user.fullName, color: kBlack70),
-              const Spacer(),
-              TextMedium(title: "${course.rating}"),
-              SizedBox(width: defaultSize),
-              Icon(
-                Icons.star,
-                color: kOrange,
-              ),
-            ],
-          ),
+        // Name and Ratings
+        SizedBox(height: defaultSize),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Name
+            TextMedium(title: course.user.fullName, color: kBlack70),
+            const Spacer(),
+            // Ratings
+            TextMedium(title: "${course.rating}"),
+            SizedBox(width: defaultSize),
+            Icon(
+              Icons.star,
+              color: kOrange,
+            ),
+          ],
+        ),
 
-          // Price
-          SizedBox(height: defaultSize),
-          TextLarge(
-            title: "N ${course.price}",
-            color: kBlack80,
-            weight: FontWeight.w600,
-          )
-        ],
-      ),
+        // Price
+        SizedBox(height: defaultSize),
+        TextLarge(
+          title: "N ${course.price}",
+          color: kBlack80,
+          weight: FontWeight.w600,
+        )
+      ],
     );
   }
 }
