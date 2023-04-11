9405d134805eda6b2c47171964cb94d66b0082c3
dc <00danzy@gmail.com>
2023-04-11 15:01:06 +0100

    refactor code to support AppsSliverAppBar Widget

--- lib/screens/Profile/profile_sceen.dart
+++ lib/screens/Profile/profile_sceen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/appbar/appbar.dart';
 import 'package:neocloud_mobile/components/bottom_navbar/apps_bottom_navbar.dart';
+import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/models/ProfileNavbarItem.dart';
 import 'package:neocloud_mobile/models/students.dart';
@@ -19,45 +21,46 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: buildAppBar(
-        title: screenName,
-        bgColor: const Color.fromRGBO(0, 0, 0, 0),
-        isDark: false,
-        actionIcon: Icons.settings,
-        routeName: getRouteName(SettingsScreen.screenName),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Stack Required Section - cover img, round bg, profile img
-            StackCoverAndProfileImage(
-              coverImg: coverImg,
-              profileImg: user.avatar,
-              coverSize: defaultSize * 27,
-              coverOverlay: kBlack.withOpacity(.2),
-              profileImgBorderSize: defaultSize * .3,
+      body: CustomScrollView(
+        slivers: [
+          buildSliverAppBar(title: screenName, bgColor: kWhite, actionIcon: Icons.settings, isDark: true, routeName: getRouteName(SettingsScreen.screenName)),
+
+
+          SliverToBoxAdapter(
+            child: Column(
+              children: <Widget>[
+                // Stack Required Section - cover img, round bg, profile img
+                StackCoverAndProfileImage(
+                  coverImg: coverImg,
+                  profileImg: user.avatar,
+                  coverSize: defaultSize * 19,
+                  coverOverlay: kBlack.withOpacity(.2),
+                  profileImgBorderSize: defaultSize * .3,
+                  profileImageVerticalPosition: defaultSize * 7.5,
+                  bgCurveVerticalPosition: defaultSize * 14,
+                ),
+
+                // User Short Intro - name, role, ratings ⭐
+                // SizedBox(height: defaultSize * 2),
+                IntroNameRoleRatings(user: user),
+
+                // Info Cards (Students, Courses, Reviews)
+                SizedBox(height: defaultSize * 2),
+                ProfileCardsList(user: user),
+
+                // Profile Navbar and Profiles Content (Courses, Activity, Info)
+                SizedBox(height: defaultSize * 3),
+                ProfileNavbarAndContent(
+                    navItems: ProfileNavbarItems.educatorItems),
+
+                SizedBox(height: defaultSize * 3),
+              ],
             ),
-
-            // User Short Intro - name, role, ratings ⭐
-            IntroNameRoleRatings(user: user),
-
-            // Info Cards (Students, Courses, Reviews)
-            SizedBox(height: defaultSize * 2),
-            ProfileCardsList(user: user),
-
-            // Profile Navbar and Profiles Content (Courses, Activity, Info)
-            SizedBox(height: defaultSize * 3),
-            ProfileNavbarAndContent(navItems: ProfileNavbarItems.educatorItems),
-
-            SizedBox(height: defaultSize * 3),
-          ],
-        ),
+          ),
+        ],
       ),
       extendBody: true,
       bottomNavigationBar: AppsBottomNavBar(),
     );
   }
 }
-
