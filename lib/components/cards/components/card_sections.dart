ad14eb92cca922b77bd374ddf147b818c6e55b14
dc <00danzy@gmail.com>
2023-04-14 06:38:30 +0100

    make displaying of labels optional

--- lib/components/cards/components/card_sections.dart
+++ lib/components/cards/components/card_sections.dart
@@ -14,33 +14,34 @@ class CardSections extends StatelessWidget {
   const CardSections({
     super.key,
     required this.data,
+    this.showLabel = true,
   });
 
   final List<Map> data;
+  final bool showLabel;
 
   @override
   Widget build(BuildContext context) {
     return Row(
         children: List.generate(data.length, (index) {
-          if (index == 0) {
-            return Expanded(
-              flex: data[index]["flex"],
-              child: buildCardSection(index),
-            );
-          }
-          return Expanded(
-            flex: data[index]["flex"],
-            child: Row(
-              children: <Widget>[
-                // Seperator
-                Seperator(),
-                // Card Section
-                buildCardSection(index),
-              ],
-            ),
-          );
-        })
-    );
+      if (index == 0) {
+        return Expanded(
+          flex: data[index]["flex"],
+          child: buildCardSection(index),
+        );
+      }
+      return Expanded(
+        flex: data[index]["flex"],
+        child: Row(
+          children: <Widget>[
+            // Seperator
+            Seperator(),
+            // Card Section
+            buildCardSection(index),
+          ],
+        ),
+      );
+    }));
   }
 
   Column buildCardSection(int index) {
@@ -48,11 +49,13 @@ class CardSections extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         // Course Label
-        TextMedium(
-          title: data[index]["label"],
-          color: kBlack70,
-          weight: FontWeight.w500,
-        ),
+        showLabel
+            ? TextMedium(
+                title: data[index]["label"],
+                color: kBlack70,
+                weight: FontWeight.w500,
+              )
+            : SizedBox(),
         // Course Title
         TextCustomMaxLine(
           title: data[index]["title"],
