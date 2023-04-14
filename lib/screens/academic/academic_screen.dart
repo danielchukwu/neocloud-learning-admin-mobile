70272e06108f7e56d2975cb51b1c43b0f2df437a
dc <00danzy@gmail.com>
2023-04-14 20:00:25 +0100

    complete attendance body

--- lib/screens/academic/academic_screen.dart
+++ lib/screens/academic/academic_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/bottom_navbar/apps_bottom_navbar.dart';
-import 'package:neocloud_mobile/components/buttons.dart';
-import 'package:neocloud_mobile/components/cards/attendance_card.dart';
 import 'package:neocloud_mobile/components/options/DisplayOptions.dart';
+// import 'package:neocloud_mobile/components/options/DisplayOptions.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/models/Options.dart';
+import 'package:neocloud_mobile/models/card_data.dart';
 import 'package:neocloud_mobile/screens/Profile/profile_sceen.dart';
 import 'package:neocloud_mobile/screens/academic/components/academic_filter_section.dart';
+import 'package:neocloud_mobile/screens/academic/components/attendance_body.dart';
 import 'package:neocloud_mobile/utils.dart';
 
 class AcademicScreen extends StatefulWidget {
@@ -35,38 +36,19 @@ class _AcademicScreenState extends State<AcademicScreen> {
             children: [
               // Academic Options (Attendance, Class routine, Courses, etc...)
               SizedBox(height: defaultSize),
-
               DisplayOptions(
                 items: AcademicOptions.items,
                 getSelectedIndex: AcademicOptions.getSelectedIndex,
-                press: (int index) {
-                  setState(() {
-                    AcademicOptions.setSelectedIndex(index);
-                  });
-                },
+                press: updateSelectedOption,
               ),
 
               // Filter Section
               SizedBox(height: defaultSize * 3),
-              const AcademicFilterSection(),
+              const AcademicFilter(),
 
               // + Take Attendance and Create Attendance Cards
               SizedBox(height: defaultSize * 3),
-              Column(
-                children: <Widget>[
-                  // + Take Attendance Button
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      buildAddButton(title: "Take Attendance", press: (context) {}),
-                    ],
-                  ),
-
-                  // Cards
-                  // AttendanceCard(data: data)
-
-                ],
-              )
+              AttendanceBody(data: attendanceList),
             ],
           ),
         ),
@@ -75,4 +57,13 @@ class _AcademicScreenState extends State<AcademicScreen> {
       bottomNavigationBar: const AppsBottomNavBar(),
     );
   }
+
+  // Options - updates option selection
+  updateSelectedOption(int index) {
+    setState(() {
+      AcademicOptions.setSelectedIndex(index);
+    });
+  }
 }
+
+
