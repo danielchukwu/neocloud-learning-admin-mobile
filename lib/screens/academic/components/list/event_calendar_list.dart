e8e7a14fb23c493814314dc1586af3bd32045586
dc <00danzy@gmail.com>
2023-04-17 11:17:36 +0100

    remove unnecesary code. only list persists

--- lib/screens/academic/components/list/event_calendar_list.dart
+++ lib/screens/academic/components/list/event_calendar_list.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/cards/event_calendar_card.dart';
-import 'package:neocloud_mobile/components/cards/faculty_card.dart';
-import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/models/card_data.dart';
 
 class EventCalendarList extends StatelessWidget {
@@ -15,23 +13,7 @@ class EventCalendarList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        // + Add Event Button
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            buildAddButton(title: "Add Event", press: (context) {}),
-          ],
-        ),
-
-        // Cards
-        SizedBox(height: defaultSize),
-        Column(
-          children: List.generate(attendanceList.length, (index) => EventCalendarCard(data: data[index])),
-        ),
-
-      ],
-    );
+          children: List.generate(data.length, (index) => EventCalendarCard(data: data[index])),
+        );
   }
 }
