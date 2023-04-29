255cdfca28e2249f68f2198c68a481dcb6eab482
dc <00danzy@gmail.com>
2023-04-29 08:56:42 +0100

    rename mainText & subText

--- lib/screens/settings/components/settings_form_header.dart
+++ lib/screens/settings/components/settings_form_header.dart
@@ -5,21 +5,21 @@ import 'package:neocloud_mobile/constraints.dart';
 class SettingsFormHeader extends StatelessWidget {
   const SettingsFormHeader({
     Key? key,
-    required this.mainText,
-    required this.subText,
+    required this.title,
+    required this.subTitle,
   }) : super(key: key);
 
-  final String mainText;
-  final String subText;
+  final String title;
+  final String subTitle;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        TextExtraLarge(title: mainText, color: kBlack90, weight: FontWeight.w500),
-        SizedBox(height: defaultSize),
-        TextMedium(title: subText, color: kBlack60),
+        TextExtraLarge(title: title, color: kBlack90, weight: FontWeight.w500),
+        subTitle.isNotEmpty ? SizedBox(height: defaultSize) : SizedBox(),
+        TextMedium(title: subTitle, color: kBlack60),
       ],
     );
   }
