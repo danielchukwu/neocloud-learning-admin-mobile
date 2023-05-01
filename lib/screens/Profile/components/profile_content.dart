995f8cb7e18facc408a2d3c24bf3dcd1fadaa9d4
dc <00danzy@gmail.com>
2023-05-01 07:06:17 +0100

    add margin top to (courses, activities and info)

--- lib/screens/Profile/components/profile_content.dart
+++ lib/screens/Profile/components/profile_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/Lists/course_list.dart';
 import 'package:neocloud_mobile/components/cards.dart';
 import 'package:neocloud_mobile/components/cards/class_works_card.dart';
 import 'package:neocloud_mobile/components/texts.dart';
@@ -17,34 +18,40 @@ class ProfileContent extends StatelessWidget {
   Widget build(BuildContext context) {
     switch (type) {
       case "courses":
-        return Column(
-            children: List.generate(
-                courses.length, (index) => CourseCard(course: courses[index])));
+        return Container(
+          margin: EdgeInsets.only(top: defaultSize * 2),
+          child: CoursesList(data: coursesList),
+        );
       case "activities":
-        return Column(
-            children: List.generate(
-              classWorksList.length,
-                  (index) => ClassWorkCard(data: classWorksList[index]),
-            ));
+        return Container(
+          margin: EdgeInsets.only(top: defaultSize * 2),
+          child: Column(
+              children: List.generate(
+            classWorksList.length,
+            (index) => ClassWorkCard(data: classWorksList[index]),
+          )),
+        );
       case "info":
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Title
-            SizedBox(height: defaultSize * 2),
-            TextCustomMaxLine(
-              title: "Account Info",
-              fontSize: defaultSize * 2.2,
-              weight: FontWeight.w600,
-              color: kBlack80,
-            ),
-
-            // Account Info Items
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: List.generate(accountInfos.length,
-                        (index) => AccountInfoTile(data: accountInfos[index]))),
-          ],
+        return Container(
+          margin: EdgeInsets.only(top: defaultSize),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Title
+              TextCustomMaxLine(
+                title: "Account Info",
+                fontSize: defaultSize * 2.2,
+                weight: FontWeight.w600,
+                color: kBlack80,
+              ),
+        
+              // Account Info Items
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List.generate(accountInfos.length,
+                      (index) => AccountInfoTile(data: accountInfos[index]))),
+            ],
+          ),
         );
       default:
         return const SizedBox();
