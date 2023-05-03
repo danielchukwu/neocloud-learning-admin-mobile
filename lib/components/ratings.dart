6379c701b65f79492df03b0f9949a73970c78ccf
dc <00danzy@gmail.com>
2023-05-03 22:18:33 +0100

    first commit

---
+++ lib/components/ratings.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/texts.dart';
+import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/screens/Profile/components/star_rating.dart';
+import 'package:neocloud_mobile/utils.dart';
+
+class Ratings extends StatelessWidget {
+  const Ratings({
+    super.key,
+    required this.rating,
+    required this.reviewsCount,
+    this.fontSize,
+    this.iconSize,
+    this.color,
+    this.showReviews = true, 
+    this.showRatingsText = true,
+  });
+
+  final double rating;
+  final int reviewsCount;
+  final double? fontSize;
+  final double? iconSize;
+  final Color? color;
+  final bool showReviews;
+  final bool showRatingsText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        // Rating
+        showRatingsText 
+        ? TextCustom(
+            title: "${rating / 20}",
+            color: color ?? kBlack80,
+            weight: FontWeight.w600,
+            fontSize: fontSize ?? defaultSize * 1.6)
+        : SizedBox(),
+
+        // Stars
+        SizedBox(width: showRatingsText ? defaultSize * .5 : 0),
+        StarRating(rating: rating, iconSize: iconSize ?? defaultSize * 2.4),
+
+        // Reviews Count
+        SizedBox(width: showReviews ? defaultSize * .5 : 0),
+        showReviews
+            ? TextCustom(
+                title:
+                    "(${reviewsCount} ${getPluralOrSingular(count: reviewsCount, word: 'review')})",
+                color: color ?? kBlack50,
+                fontSize: fontSize ?? defaultSize * 1.6,
+              )
+            : SizedBox(),
+      ],
+    );
+    ;
+  }
+}
