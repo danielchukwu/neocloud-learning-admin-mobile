0773ef572bd145fee7d91626a1ae44333b103b35
dc <00danzy@gmail.com>
2023-05-05 22:58:03 +0100

    use SliverAppBar

--- lib/screens/class_works/components/class_works_body.dart
+++ lib/screens/class_works/components/class_works_body.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/cards/class_works_card.dart';
 import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/models/Students.dart';
 import 'package:neocloud_mobile/models/card_data.dart';
+import 'package:neocloud_mobile/screens/Profile/profile_sceen.dart';
+
+import '../../academic/academic_screen.dart';
 
 class ClassWorkBody extends StatefulWidget {
   const ClassWorkBody({Key? key}) : super(key: key);
@@ -38,34 +42,46 @@ class _ClassWorkBodyState extends State<ClassWorkBody> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: screenPadding,
-      child: Column(
-        children: [
-          // Search Text Field
-          SizedBox(height: defaultSize * 2),
-          buildSearchTextField(press: updateSearchController),
-
-          // + Add Button
-          SizedBox(height: defaultSize * 3),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+    return CustomScrollView(slivers: <Widget>[
+      buildSliverAppBar(
+        title: AcademicScreen.screenName,
+        bgColor: kWhite,
+        isDark: true,
+        showLeading: false,
+        routeWidget1: ProfileScreen(user: users[0]),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: screenPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              buildAddButton(title: "Add Class Work", press: (context) {}),
+              // Search Text Field
+              SizedBox(height: defaultSize * 2),
+              buildSearchTextField(press: updateSearchController),
+
+              // + Add Button
+              SizedBox(height: defaultSize * 3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  buildAddButton(title: "Add Class Work", press: (context) {}),
+                ],
+              ),
+
+              // Cards
+              SizedBox(height: defaultSize),
+              Column(
+                children: List.generate(classWorksList.length,
+                    (index) => ClassWorkCard(data: classWorksList[index])),
+              ),
+
+              // Bottom Padding
+              pageBottomPadding(),
             ],
           ),
-
-          // Cards
-          SizedBox(height: defaultSize),
-          Column(
-            children: List.generate(classWorksList.length,
-                    (index) => ClassWorkCard(data: classWorksList[index])),
-          ),
-
-          // Bottom Padding
-          pageBottomPadding(),
-        ],
+        ),
       ),
-    );
+    ]);
   }
 }
