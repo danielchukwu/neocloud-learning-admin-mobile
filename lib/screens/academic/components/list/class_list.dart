1244c692ede33593fc9035126d92033dcb18e83c
dc <00danzy@gmail.com>
2023-05-18 11:44:23 +0100

    improve

--- lib/screens/academic/components/list/class_list.dart
+++ lib/screens/academic/components/list/class_list.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/cards/class_card.dart';
+import 'package:neocloud_mobile/models/Class.dart';
 
 import 'package:neocloud_mobile/models/card_data.dart';
 
 class ClassList extends StatelessWidget {
   const ClassList({
     super.key,
-    required this.data,
+    required this.clas,
   });
 
-  final List<Class> data;
+  final List<Class> clas;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children:
-          List.generate(data.length, (index) => ClassCard(data: data[index])),
+      children: 
+          List.generate(clas.length, (index) => ClassCard(clas: clas[index])),
     );
   }
 }
