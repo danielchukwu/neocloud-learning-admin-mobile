9c03435311d169db184356c9a44f6f9dcb342706
dc <00danzy@gmail.com>
2023-05-18 11:45:10 +0100

    improve

--- lib/screens/academic/components/list/faculty_list.dart
+++ lib/screens/academic/components/list/faculty_list.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/cards/faculty_card.dart';
+import 'package:neocloud_mobile/models/Faculty.dart';
 import 'package:neocloud_mobile/models/card_data.dart';
 
 class FacultyList extends StatelessWidget {
   const FacultyList({
     super.key,
-    required this.data,
+    required this.faculty,
   });
 
-  final List<FacultyOld> data;
+  final List<Faculty> faculty;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-          children: List.generate(data.length, (index) => FacultyCard(data: data[index])),
+          children: List.generate(faculty.length, (index) => FacultyCard(faculty: faculty[index])),
         );
   }
 }
