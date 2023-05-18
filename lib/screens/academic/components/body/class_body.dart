b7c226f6737253cdea74c4751ec2d4bf830ddae8
dc <00danzy@gmail.com>
2023-05-18 11:43:55 +0100

    remove Add Button from new design

--- lib/screens/academic/components/body/class_body.dart
+++ lib/screens/academic/components/body/class_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:neocloud_mobile/components/cards/class_card.dart';
 import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/models/Class.dart';
 import 'package:neocloud_mobile/models/card_data.dart';
 
 class ClassBody extends StatelessWidget {
@@ -10,19 +11,10 @@ class ClassBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        // + Add Class Button
-        SizedBox(height: defaultSize * 3),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            buildAddButton(title: "Add Class", press: (context) {}),
-          ],
-        ),
-
         // Cards
         SizedBox(height: defaultSize),
         Column(
-          children: List.generate(classesList.length, (index) => ClassCard(data: classesList[index])),
+          children: List.generate( classesList.length, (index) => ClassCard(clas: classesList[index])),
         ),
       ],
     );
