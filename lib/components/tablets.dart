a3d85fe7a254a1848bcd43edec33b130b7868b6f
dc <00danzy@gmail.com>
2023-06-08 21:47:15 +0100

    add TextColorTablet widget

---
+++ lib/components/tablets.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/texts.dart';
+import 'package:neocloud_mobile/constraints.dart';
+
+class TextColorTablet extends StatelessWidget {
+  const TextColorTablet({
+    super.key,
+    required this.title,
+    required this.bgColor,
+    this.color = Colors.white,
+  });
+
+  final String title;
+  final Color color;
+  final Color bgColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: defaultSize * .5, horizontal: defaultSize),
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(defaultSize * .5)
+      ),
+      child: TextSmall(title: title, color: color, weight: FontWeight.w500,),
+    );
+  }
+}
