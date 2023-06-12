12c380a971b2044282e15b76aadc7643c46014a0
dc <00danzy@gmail.com>
2023-06-12 22:55:34 +0100

    use kBlack79 for all IconText colors

--- lib/screens/course/components/course_outline.dart
+++ lib/screens/course/components/course_outline.dart
@@ -57,14 +57,17 @@ class _CourseOutlineState extends State<CourseOutline> {
         IconText(
             title: 'Modules (45)',
             icon: Icons.featured_play_list_sharp,
+            iconColor: kBlack70,
             fontSize: defaultSize * 1.4),
         IconText(
             title: 'Lectures (158)',
             icon: Icons.video_library,
+            iconColor: kBlack70,
             fontSize: defaultSize * 1.4),
         IconText(
             title: 'Time (115hrs)',
             icon: Icons.watch_later,
+            iconColor: kBlack70,
             fontSize: defaultSize * 1.4)
       ],
     );
