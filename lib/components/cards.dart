4a6d9108c85e97d531792edeb982fc84a581157b
dc <00danzy@gmail.com>
2023-06-15 23:17:37 +0100

    improve card, widen image to full screen width and pad textarea

--- lib/components/cards.dart
+++ lib/components/cards.dart
@@ -28,41 +28,50 @@ class CourseCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           // Course Image
-          RectangularBoxImage(img: course.avatar, height: defaultSize * 20),
+          RectangularBoxImage(img: course.avatar, height: defaultSize * 22),
 
-          // Title
-          SizedBox(height: defaultSize * 2),
-          TextCustom(
-            title: course.title,
-            fontSize: defaultSize * 2.2,
-            color: kBlack80,
-            weight: FontWeight.w700,
-          ),
-
-          // Name and Ratings
+          // Course Body - Title, tutor, price
           SizedBox(height: defaultSize),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Name
-              TextMedium(title: course.user.fullName, color: kBlack70),
-              const Spacer(),
-              // Ratings
-              TextMedium(title: getRatingFormat(course.rating)),
-              SizedBox(width: defaultSize),
-              Icon(
-                Icons.star,
-                color: kOrange,
-              ),
-            ],
-          ),
-
-          // Price
-          SizedBox(height: defaultSize),
-          TextExtraLarge(
-            title: "N ${getMoneyFormat(course.price)}",
-            color: kBlack80,
-            weight: FontWeight.w600,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: defaultSize * 1.5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Title
+                TextCustom(
+                  title: course.title,
+                  fontSize: defaultSize * 2.2,
+                  color: kBlack80,
+                  weight: FontWeight.w700,
+                ),
+          
+                // Name and Ratings
+                SizedBox(height: defaultSize * .5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Name
+                    TextMedium(title: course.user.fullName, color: kBlack70),
+                    const Spacer(),
+                    // Ratings
+                    TextMedium(title: getRatingFormat(course.rating)),
+                    SizedBox(width: defaultSize),
+                    Icon(
+                      Icons.star,
+                      color: kOrange,
+                    ),
+                  ],
+                ),
+          
+                // Price
+                SizedBox(height: defaultSize * .5),
+                TextExtraLarge(
+                  title: "N ${getMoneyFormat(course.price)}",
+                  color: kBlack80,
+                  weight: FontWeight.w600,
+                ),
+              ],
+            ),
           ),
 
           SizedBox(height: defaultSize * 3),
