516fba7b292d056430b66ea5871f5afa06b67fea
dc <00danzy@gmail.com>
2023-06-15 23:15:47 +0100

    improve

--- lib/models/Options.dart
+++ lib/models/Options.dart
@@ -1,7 +1,13 @@
 class Option {
   final String title;
+  // This will be used for knowing when to show current data for an option or old data
+  // e.g current classworks and old classworks. if this is true, then we'll show current classworks else old classworks
+  bool selectedCurrent;
 
-  const Option({required this.title});
+  Option({
+    required this.title, 
+    this.selectedCurrent = true
+  });
 }
 
 // This will be extended for the purpose of type checking
@@ -20,21 +26,5 @@ class AcademicOptions extends OptionsObject {
     Option(title: "Faculties"),
     Option(title: "Classworks"),
     Option(title: "Class Schedules"),
-  ];
-}
-
-class BackOfficeOptions extends OptionsObject {
-  static int selectedIndex = 0;
-
-  static setSelectedIndex(int index) => selectedIndex = index;
-  static int getSelectedIndex() => selectedIndex;
-  // static int get selectedOption => _selectedOption;
-
-  // Try to make sure the items are always divisible by 2
-  static List<Option> items = [
-    const Option(title: "Books"),
-    const Option(title: "Session Manager"),
-    const Option(title: "Noticeboard"),
-    const Option(title: "Enquires"),
   ];
 }
