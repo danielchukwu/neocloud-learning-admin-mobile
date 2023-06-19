018fd084be65480d92adf64c5a568a31c2912a7f
dc <00danzy@gmail.com>
2023-06-19 22:32:55 +0100

    improve code

--- lib/screens/Profile/components/profile_content.dart
+++ lib/screens/Profile/components/profile_content.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:neocloud_mobile/components/Lists/course_list.dart';
-import 'package:neocloud_mobile/components/buttons.dart';
 import 'package:neocloud_mobile/components/cards.dart';
-import 'package:neocloud_mobile/components/cards/class_works_card.dart';
+// import 'package:neocloud_mobile/components/images.dart';
+// import 'package:neocloud_mobile/components/tablets.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/models/AccountInfo.dart';
-import 'package:neocloud_mobile/models/Courses.dart';
-import 'package:neocloud_mobile/models/card_data.dart';
-import 'package:neocloud_mobile/screens/Profile/components/account_info_tile.dart';
+// import 'package:neocloud_mobile/models/AccountInfo.dart';
+import 'package:neocloud_mobile/models/Class.dart';
+import 'package:neocloud_mobile/models/Students.dart';
+// import 'package:neocloud_mobile/screens/Profile/components/account_info_tile.dart';
 import 'package:neocloud_mobile/screens/Profile/components/profile_dashboard.dart';
+import 'package:neocloud_mobile/utils.dart';
 
 class ProfileContent extends StatelessWidget {
   const ProfileContent({Key? key, required this.type}) : super(key: key);
@@ -22,38 +22,76 @@ class ProfileContent extends StatelessWidget {
       case "dashboard":
         return const ProfileDashboard();
       case "info":
-        return Container(
-          margin: EdgeInsets.only(top: defaultSize * 2),
-          child: Column(
-              children: List.generate(
-            classWorksList.length,
-            (index) => ClassWorkCard(clas: classWorksList[index]),
-          )),
-        );
+        return const ProfileAbout();
       case "classes":
-        return Container(
-          margin: EdgeInsets.only(top: defaultSize),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              // Title
-              TextCustomMaxLine(
-                title: "Account Info",
-                fontSize: defaultSize * 2.2,
-                weight: FontWeight.w600,
-                color: kBlack80,
-              ),
-
-              // Account Info Items
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: List.generate(accountInfos.length,
-                      (index) => AccountInfoTile(data: accountInfos[index]))),
-            ],
-          ),
-        );
+        return const ProfileClasses();
       default:
         return const SizedBox();
     }
+  }
+}
+
+class ProfileClasses extends StatelessWidget {
+  const ProfileClasses({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(
+        classesList.length,
+        (index) => ClassCard(clas: classesList[index]),
+      ),
+    );
+  }
+}
+
+class ProfileAbout extends StatelessWidget {
+  const ProfileAbout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var user = users[9];
+
+    return Container(
+      padding: screenPadding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // About Section
+          SizedBox(height: defaultSize * 1.5),
+          TextLarge(title: 'ABOUT', weight: FontWeight.w500, color: kBlack80),
+          // Body
+          SizedBox(height: defaultSize),
+          TextMedium(
+            title: user.about,
+            color: kBlack80,
+          ),
+
+          // Roles Section
+          SizedBox(height: defaultSize * 3),
+          TextLarge(title: 'ROLES', weight: FontWeight.w500, color: kBlack80),
+
+          // Icons List
+          SizedBox(height: defaultSize),
+          Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: List.generate(
+              user.role.length,
+              (index) => IconText(
+                title: user.role[index],
+                svg: getRoleSvgFileName(roleList: [user.role[index]]),
+                iconIsLeft: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
