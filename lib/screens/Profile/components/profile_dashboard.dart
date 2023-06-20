4c20caa08d7796df5aa04230ed662b13161c4fa0
dc <00danzy@gmail.com>
2023-06-20 23:26:54 +0100

    improve code

--- lib/screens/Profile/components/profile_dashboard.dart
+++ lib/screens/Profile/components/profile_dashboard.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/Lists/class_schedule_body.dart';
+import 'package:neocloud_mobile/components/Lists/class_works_list.dart';
 import 'package:neocloud_mobile/components/buttons.dart';
 import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/models/ClassSchedule.dart';
+import 'package:neocloud_mobile/models/ClassWork.dart';
 import 'package:neocloud_mobile/screens/Profile/components/current-old-header.dart';
-import 'package:neocloud_mobile/screens/academic/components/body/class_schedule_body.dart';
-import 'package:neocloud_mobile/screens/academic/components/body/class_works_body.dart';
 
 class ProfileDashboard extends StatefulWidget {
   const ProfileDashboard({
@@ -17,7 +19,7 @@ class ProfileDashboard extends StatefulWidget {
 class _ProfileDashboardState extends State<ProfileDashboard> {
   var selectedIndex = 0;
   var sections = ['Schedule', 'CWs'];
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,18 +30,27 @@ class _ProfileDashboardState extends State<ProfileDashboard> {
           buildNavbar(),
 
           // Current - Old Navbar
-          CurrentOldHeader(pressRight: () {}, pressLeft: () {}, title: sections[selectedIndex], rightText: 'Old', selectedLeft: true),
+          CurrentOldHeader(
+              pressRight: () {},
+              pressLeft: () {},
+              title: sections[selectedIndex],
+              rightText: 'Old',
+              selectedLeft: true),
 
-          // List 
-          selectedIndex == 0 
-            ? ClassSchedulesList()
-            : SizedBox(),
-          
-          selectedIndex == 1 
-            ? ClassworksList()
-            : SizedBox(),
+          // List
+          selectedIndex == 0
+              ? ClassSchedulesList(
+                  classScheduleList: classScheduleList,
+                )
+              : SizedBox(),
 
-          // selectedIndex == 2 
+          selectedIndex == 1
+              ? ClassworksList(
+                  classworksList: classWorksList,
+                )
+              : SizedBox(),
+
+          // selectedIndex == 2
           //   ? ClassworksList()
           //   : SizedBox(),
         ],
@@ -49,30 +60,31 @@ class _ProfileDashboardState extends State<ProfileDashboard> {
 
   Padding buildNavbar() {
     return Padding(
-          padding: EdgeInsets.symmetric(horizontal: defaultSize),
-          child: Row(
-            children: List.generate(
-              sections.length,
-              (index) => Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: defaultSize * .5),
-                  child: AppsButton(
-                    title: sections[index],
-                    padTopBottom: 0,
-                    padLeftRight: 0,
-                    weight: FontWeight.w600,
-                    color: selectedIndex == index ? kWhite : kBlack70,
-                    bgColor: selectedIndex == index ? kBlack80 : kBlack.withOpacity(.1),
-                    press: (context) {
-                      setState((){
-                        selectedIndex = index;
-                      });
-                    },
-                  ),
-                ),
+      padding: EdgeInsets.symmetric(horizontal: defaultSize),
+      child: Row(
+        children: List.generate(
+          sections.length,
+          (index) => Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: defaultSize * .5),
+              child: AppsButton(
+                title: sections[index],
+                padTopBottom: 0,
+                padLeftRight: 0,
+                weight: FontWeight.w600,
+                color: selectedIndex == index ? kWhite : kBlack70,
+                bgColor:
+                    selectedIndex == index ? kBlack80 : kBlack.withOpacity(.1),
+                press: (context) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
               ),
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
