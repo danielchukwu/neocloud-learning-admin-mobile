76543e694e737f8ee9ff617b220ecbe93e2ceecd
dc <00danzy@gmail.com>
2023-06-20 23:26:36 +0100

    add top and bottom border

--- lib/screens/dashboard/components/personel_navbar.dart
+++ lib/screens/dashboard/components/personel_navbar.dart
@@ -16,36 +16,42 @@ class PersonelNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: List<Widget>.generate(
-        navItems.length,
-        (index) => InkWell(
-          onTap: () => press(index),
-          highlightColor: kWhite,
-          splashColor: appsSplashColor,
-          radius: appsSplashRadius,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(defaultSize * 2, defaultSize * .5, defaultSize * 2, 0),
-            child: Column(
-              children: <Widget>[
-                // Title
-                TextLarge(
-                  title: navItems[index] + "s",
-                  weight: FontWeight.w500,
-                  color: index == selectedIndex ? kBlack90 : kBlack50,
-                ),
-                // Selector
-                SizedBox(height: defaultSize * .7),
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 100),
-                  curve: Curves.easeIn,
-                  height: defaultSize * .4,
-                  width: defaultSize * 4,
-                  color: index == selectedIndex
-                      ? kBlack.withOpacity(.5)
-                      : Colors.transparent,
-                )
-              ],
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(bottom: appsBorder, top: appsBorder),
+      ),
+      child: Row(
+        children: List<Widget>.generate(
+          navItems.length,
+          (index) => InkWell(
+            onTap: () => press(index),
+            highlightColor: kWhite,
+            splashColor: appsSplashColor,
+            radius: appsSplashRadius,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  defaultSize * 2, defaultSize * 1.5, defaultSize * 2, 0),
+              child: Column(
+                children: <Widget>[
+                  // Title
+                  TextLarge(
+                    title: navItems[index] + "s",
+                    weight: FontWeight.w500,
+                    color: index == selectedIndex ? kBlack90 : kBlack50,
+                  ),
+                  // Selector
+                  SizedBox(height: defaultSize * 1),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeIn,
+                    height: defaultSize * .4,
+                    width: defaultSize * 4,
+                    color: index == selectedIndex
+                        ? kBlack.withOpacity(.5)
+                        : Colors.transparent,
+                  )
+                ],
+              ),
             ),
           ),
         ),
