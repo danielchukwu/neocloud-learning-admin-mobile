8a4e345e54b099ca06ce44baaef529647df791fb
dc <00danzy@gmail.com>
2023-06-20 23:24:01 +0100

    user classes cards instead of courses

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:neocloud_mobile/components/Lists/course_list.dart';
+import 'package:neocloud_mobile/components/Lists/class_list.dart';
 import 'package:neocloud_mobile/components/Lists/user_list.dart';
 import 'package:neocloud_mobile/components/bottom_navbar/apps_bottom_navbar.dart';
-import 'package:neocloud_mobile/components/images.dart';
-import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/models/Courses.dart';
+import 'package:neocloud_mobile/models/Class.dart';
 import 'package:neocloud_mobile/models/Students.dart';
-import 'package:neocloud_mobile/screens/Profile/profile_sceen.dart';
-import 'package:neocloud_mobile/screens/search/components/search_field.dart';
 import 'package:neocloud_mobile/screens/search/components/search_filter.dart';
-import 'package:neocloud_mobile/utils.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({
@@ -23,10 +18,15 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final TextEditingController _searchController = TextEditingController(text: "");
-  List<String> searchFilter = ["All", "Educators", "Students", "Courses"];
+  final TextEditingController _searchController =
+      TextEditingController(text: "");
+  List<String> searchFilter = [
+    "All",
+    "Educators",
+    "Students",
+    "Classes",
+  ];
   int _selectedIndex = 0;
-
 
   @override
   void initState() {
@@ -68,24 +68,47 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
 
               // Filter
-              SearchFilter(searchFilter: searchFilter, selectedIndex: _selectedIndex, updateIndex: updateIndex),
+              SearchFilter(
+                searchFilter: searchFilter,
+                selectedIndex: _selectedIndex,
+                updateIndex: updateIndex,
+              ),
 
               // Result
               SizedBox(height: defaultSize),
               Padding(
-                padding: _selectedIndex != 3 ?  EdgeInsets.fromLTRB(defaultSize * 2, 0, defaultSize * 2, defaultSize * 2) : EdgeInsets.all(0),
+                padding: _selectedIndex != 3
+                    ? EdgeInsets.fromLTRB(
+                        defaultSize * 2, 0, defaultSize * 2, defaultSize * 2)
+                    : EdgeInsets.all(0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     // Users List
-                  UserList(usersList: users.where((user) => (_selectedIndex == 0)).toList()),
-                  UserList(usersList: users.where((user) => (user.role.contains("Educator") && _selectedIndex == 1)).toList()),
-                  UserList(usersList: users.where((user) => (user.role.contains("Student") && _selectedIndex == 2)).toList()),
+                    UserList(
+                        usersList: users
+                            .where((user) => (_selectedIndex == 0))
+                            .toList()),
+                    UserList(
+                        usersList: users
+                            .where((user) => (user.role.contains("Educator") &&
+                                _selectedIndex == 1))
+                            .toList()),
+                    UserList(
+                        usersList: users
+                            .where((user) => (user.role.contains("Student") &&
+                                _selectedIndex == 2))
+                            .toList()),
 
-                  // Courses List
-                  _selectedIndex == 3 ? CoursesList(coursesList: coursesList) : SizedBox(),
-              
-                  SizedBox(height: defaultSize * 3),
+                    // Classes List
+                    _selectedIndex == 3
+                        ? ClassList(
+                            classList: classesList,
+                            showClassAvatar: true,
+                          )
+                        : SizedBox(),
+
+                    SizedBox(height: defaultSize * 3),
                   ],
                 ),
               )
@@ -97,5 +120,3 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 }
-
-
