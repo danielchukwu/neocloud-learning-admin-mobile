2074f38a060ef7e359ac0f885681b1f39a441b31
dc <00danzy@gmail.com>
2023-07-31 21:59:15 +0100

    first commit

---
+++ lib/graphql/models/UsersFacultiesRolesModel.dart
@@ -0,0 +1,53 @@
+import 'package:neocloud_mobile/graphql/models/FacultyModel.dart';
+import 'package:neocloud_mobile/graphql/models/RoleModel.dart';
+import 'package:neocloud_mobile/graphql/models/UserModel.dart';
+// : (){} "" <> ? _ ! *
+
+class usersFacultiesRolesModel {
+  final String id;
+  final FacultyModel faculty;
+  final RoleModel role;
+  final UserModel user;
+  // clas: User!
+
+  usersFacultiesRolesModel({
+    required this.id,
+    required this.faculty,
+    required this.role,
+    required this.user,
+  });
+
+  static usersFacultiesRolesModel fromMap({required Map ufr}) => usersFacultiesRolesModel(
+    id: ufr['_id'],
+    user: UserModel(
+      id: ufr['user']['_id'],
+      name: ufr['user']['name'],
+    ),
+    role: RoleModel(
+      id: ufr['role']['_id'],
+      name: ufr['role']['name'],
+    ),
+    faculty: FacultyModel(
+      id: ufr['faculty']['_id'],
+      name: ufr['faculty']['name'],
+    ),
+  );
+}
+
+// query Query {
+//   usersFacultiesRoles {
+//     _id
+//     faculty {
+//       _id
+//       name
+//     }
+//     role {
+//       _id
+//       name
+//     }
+//     user {
+//       _id
+//       name
+//     }
+//   }
+// }
---
+++ lib/graphql/services/class_instance_service.dart
@@ -0,0 +1,53 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:neocloud_mobile/graphql/graphql_config.dart';
+import 'package:neocloud_mobile/graphql/models/ClassInstanceModel.dart';
+
+class ClassInstanceService {
+  static var config = GraphQLConfig();
+  var client = config.client;
+
+  Future<List<ClassInstanceModel>> getClassInstances({int? limit}) async {
+    String ciQuery = """
+      query ClassInstances(\$limit: Int) {
+        classInstances(limit: \$limit) {
+          _id
+          isCompleted
+          class {
+            _id
+            name
+          }
+          educator {
+            _id
+            name
+          }
+        }
+      }
+    """;
+
+    try {
+      var result = await client.query(QueryOptions(
+        document: gql(ciQuery),
+        variables: {'limit': limit ?? 100},
+      ));
+
+      if (result.hasException) {
+        throw Exception(result.exception);
+      } else {
+        List? classInstances = result.data?['classInstances'];
+
+        if (classInstances == null) {
+          return [];
+        }
+
+        List<ClassInstanceModel> ciList =
+            classInstances.map((ci) => ClassInstanceModel.fromMap(ci: ci)).toList();
+        print('classInstances');
+        print(ciList);
+
+        return ciList;
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
