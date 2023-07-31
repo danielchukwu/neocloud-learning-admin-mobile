5b93c179e601ef745f1f69094a34d09ccbed5973
dc <00danzy@gmail.com>
2023-07-31 22:04:16 +0100

    update: use ClassworkModel for GraphQL fetch

--- lib/screens/classwork/classwork_screen.dart
+++ lib/screens/classwork/classwork_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/cards/class_works_card.dart';
 import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/graphql/models/ClassworkModel.dart';
 import 'package:neocloud_mobile/models/ClassWork.dart';
 
 class ClassWorkScreen extends StatelessWidget {
   const ClassWorkScreen({super.key, required this.classwork});
   static String screenName = 'Classwork';
 
-  final ClassWork classwork;
+  final ClassworkModel classwork;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,7 @@ class ClassWorkScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               ClassWorkCard(
-                clas: classwork,
+                classwork: classwork,
                 showFeedback: true,
                 enableGestureDecorator: false,
               ),
