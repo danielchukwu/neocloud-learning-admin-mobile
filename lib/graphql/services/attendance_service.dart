4f5089de208e8cf6d6dfddc8faced35087ed278e
dc <00danzy@gmail.com>
2023-07-31 21:59:27 +0100

    first commit

---
+++ lib/graphql/services/attendance_service.dart
@@ -0,0 +1,56 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:neocloud_mobile/graphql/graphql_config.dart';
+import 'package:neocloud_mobile/graphql/models/AttendanceModel.dart';
+
+class AttendanceService {
+  static var config = GraphQLConfig();
+  var client = config.client;
+
+  Future<List<AttendanceModel>> getAtendances({int? limit}) async {
+    String attQuery = """
+      query ExampleQuerys(\$limit: Int) {
+        attendances(limit: \$limit) {
+          _id
+          class {
+            _id
+            name
+          }
+          classSchedule {
+            _id
+            title
+          }
+          user {
+            _id
+            name
+          }
+        }
+      }
+    """;
+
+    try {
+      var result = await client.query(QueryOptions(
+        document: gql(attQuery),
+        variables: {'limit': limit ?? 100},
+      ));
+
+      if (result.hasException) {
+        throw Exception(result.exception);
+      } else {
+        List? attendances = result.data?['attendances'];
+
+        if (attendances == null) {
+          return [];
+        }
+
+        List<AttendanceModel> attList =
+            attendances.map((cw) => AttendanceModel.fromMap(att: cw)).toList();
+        print('attendances');
+        print(attList);
+
+        return attList;
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
---
+++ lib/graphql/services/notification_service.dart
@@ -0,0 +1,70 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:neocloud_mobile/graphql/graphql_config.dart';
+import 'package:neocloud_mobile/graphql/models/NotificationModel.dart';
+
+class NotificationService {
+  static var config = GraphQLConfig();
+  var client = config.client;
+
+  Future<List<NotificationModel>> getNotifications({int? limit}) async {
+    String notiQuery = """
+      query Query(\$limit: Int) {
+        notifications(limit: \$limit) {
+          _id
+          body
+          classInstance {
+            _id
+            isCompleted
+            class {
+              _id
+              name
+            }
+          }
+          classwork {
+            _id
+            title
+          }
+          faculty {
+            _id
+            name
+          }
+          notificationType {
+            _id
+            name
+          }
+          user {
+            _id
+            avatar
+            name
+          }
+        }
+      }
+    """;
+
+    try {
+      var result = await client.query(QueryOptions(
+        document: gql(notiQuery),
+        variables: {'limit': limit ?? 100},
+      ));
+
+      if (result.hasException) {
+        throw Exception(result.exception);
+      } else {
+        List? notifications = result.data?['notifications'];
+
+        if (notifications == null) {
+          return [];
+        }
+
+        List<NotificationModel> notiList =
+            notifications.map((notification) => NotificationModel.fromMap(n: notification)).toList();
+        print('notifications');
+        print(notiList);
+
+        return notiList;
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
