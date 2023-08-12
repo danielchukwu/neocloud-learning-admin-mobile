9071d1f7842f421136cd5db728f4d8b137a998b5
dc <00danzy@gmail.com>
2023-08-12 23:41:22 +0100

    update: add showTopAlertDialog and showCenterAlertDialog functions

---
+++ lib/components/popups.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/texts.dart';
+import 'package:neocloud_mobile/constraints.dart';
+
+
+showTopAlertDialog(BuildContext context, { required String text, bool isError = true}) {
+  // Animation Controller
+  AnimationController controller = AnimationController(
+    vsync: Navigator.of(context)!.overlay!,
+    duration: Duration(milliseconds: 150),
+  );
+
+  OverlayEntry overlayEntry;
+
+  overlayEntry = OverlayEntry(builder: (context) {
+    controller.forward();
+    
+    return Positioned(
+      child: FadeTransition(
+        opacity: Tween<double>(begin: 0, end: 1).animate(controller),
+        child: SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset(0, -.43),
+            end: Offset(0, -.40),
+          ).animate(controller),
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: defaultSize * 1, horizontal: defaultSize * 1.5),
+              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width / 1.3,),
+              decoration: BoxDecoration(
+                color:  isError ? const Color(0xFFeb4d4b) : const Color(0xFF1dd1a1),
+                borderRadius: BorderRadius.all(Radius.circular(defaultSize * .8))
+              ),
+              child: TextMedium(title: text, color: kWhite, textAlign: TextAlign.center),
+            ),
+          ),
+        ),
+      ),
+    );
+  });
+
+  Future.delayed(Duration(seconds: 3), () {
+    controller.reverse();
+    Future.delayed(Duration(milliseconds: 500), () => overlayEntry.remove());
+  });
+
+  Overlay.of(context).insert(overlayEntry);
+}
+
+
+showCenterAlertDialog(BuildContext context) {
+  return showDialog(
+    // barrierColor: Colors.transparent,
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(defaultSize * .5)),
+            color: kRed,
+          ),
+          child: TextMedium(
+            title: 'An Error Just Occured!',
+          ),
+        ),
+      );
+    },
+  );
+}
