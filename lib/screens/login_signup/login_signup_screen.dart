31da0d01e3260a9ce70755b52b0ffa0c4b3e8931
dc <00danzy@gmail.com>
2023-08-12 23:48:30 +0100

    Improve

--- lib/screens/login_signup/login_signup_screen.dart
+++ lib/screens/login_signup/login_signup_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:neocloud_mobile/components/buttons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:neocloud_mobile/app_secure_storage.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/screens/login_signup/components/appbar_leading_icon.dart';
-import 'package:neocloud_mobile/screens/login_signup/components/intro_text.dart';
 import 'package:neocloud_mobile/screens/login_signup/components/login_form.dart';
 import 'package:neocloud_mobile/screens/login_signup/components/signup_form.dart';
 
@@ -17,63 +16,76 @@ class LoginSignupScreen extends StatefulWidget {
 
 class _LoginSignupScreenState extends State<LoginSignupScreen> {
   bool showLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // remove jwt tokens
+    AppSecureStorage.deleteToken();
+    AppSecureStorage.deleteRefreshToken();
+  }
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
+    double getScreenHeight() {
+      var screenHeight = MediaQuery.of(context).size.height;
+      if (screenHeight < 800) return 800;
+      return screenHeight;
+    }
 
     return Scaffold(
-        appBar: buildAppBar(),
-        body: SingleChildScrollView(
-          padding: screenPadding,
-          child: Container(
-            // color: Colors.red,
-            constraints: BoxConstraints(maxHeight: screenHeight / 1.12),
-            child: Column(
-              children: <Widget>[
-                // Section 1 - Intro Text'
-                Spacer(),
-                const IntroText(),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: screenPadding,
+            child: Container(
+              constraints: BoxConstraints(maxHeight: getScreenHeight() / 1.03),
+              child: Column(
+                children: <Widget>[
+                  // Logo
+                  // Image(image: AssetImage(nctFaviconBlue), width: 50,),
+                  Spacer(),
+                  SvgPicture.asset(nctFaviconBlue, width: 70, color: kBlueLight,),
+        
+                  // Section 1 - Intro Text'
+                  SizedBox(height: defaultSize),
+                  TextExtraLarge(title: showLogin ? "Log into your Account" : "Create your Account", weight: FontWeight.w600),
+        
+                  // Section 2 - Form
+                  SizedBox(height: defaultSize),
+                  showLogin ? LoginForm() : SignupForm(),
+        
+                  // Section 3 - switch forms
+                  Spacer(),
+                  Center(
+                    child: TextMedium(
+                        title: showLogin
+                            ? "Don't have an account?"
+                            : "Already have an account?",
+                        color: kBlack.withOpacity(.4)),
+                  ),
 
-                // Section 2 - Form
-                SizedBox(height: defaultSize),
-                showLogin ? LoginForm() : SignupForm(),
-
-                // Section 3 - switch forms
-                Spacer(),
-                Center(
-                  child: TextMedium(
-                      title: showLogin
-                          ? "Don't have an account?"
-                          : "Already have an account?",
-                      color: kBlack.withOpacity(.4)),
-                ),
-                SizedBox(height: defaultSize * 1.5),
-                // AppsButton(title: showLogin ? 'Sign up' : 'Login', press: (context) => setState(() => showLogin = !showLogin) ),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      OutlinedButton(
-                        onPressed: () => setState(() => showLogin = !showLogin),
-                        child: TextMedium(title: showLogin ? 'Sign up' : 'Login'),
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all(
-                            EdgeInsets.symmetric(vertical: defaultSize * 1.2),
+                  SizedBox(height: defaultSize),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () => setState(() => showLogin = !showLogin),
+                          child: Container(
+                            constraints: BoxConstraints(minHeight: defaultSize * 5.5, maxHeight: defaultSize * 5.5),        
+                            child: Center(child: TextMedium(title: showLogin ? 'Sign up' : 'Login')),
                           ),
-                        ),
-                      )
-                    ]),
-
-                SizedBox(height: defaultSize * 2),
-              ],
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(borderRadius: appsBorderRadius)
+                            ),
+                          ),
+                        )
+                      ]),
+        
+                  SizedBox(height: defaultSize * 2),
+                ],
+              ),
             ),
           ),
         ));
-  }
-
-  PreferredSize buildAppBar() {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(defaultSize * 5.8),
-      child: AppBar(leading: const LoginAppBarBackButton()),
-    );
   }
 }
