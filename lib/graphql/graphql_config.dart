ae9d80caf54081fa6f20a6420e8bf056368b441d
dc <00danzy@gmail.com>
2023-08-12 23:51:26 +0100

    implement: client updates the Authorization header 'Bearer token' whenever our token is updated

--- lib/graphql/graphql_config.dart
+++ lib/graphql/graphql_config.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:neocloud_mobile/app_secure_storage.dart';
 
 class GraphQLConfig {
-  static HttpLink link = HttpLink('https://neocloud-school-portal-bk.onrender.com/graphql');
+  static HttpLink httpLink = HttpLink('https://neocloud-school-portal-bk.onrender.com/graphql');
 
-  GraphQLClient client = GraphQLClient(cache: GraphQLCache(), link: link);
+  static final AuthLink authLink = AuthLink(
+    getToken: () async {
+      String token = await AppSecureStorage.getToken();
+      return 'Bearer $token';
+    },
+    headerKey: 'Authorization',
+  );
+
+  static final Link link = authLink.concat(httpLink);
+  
+  
+  // GraphQLClient client = GraphQLClient(cache: GraphQLCache(), link: link);
+  final ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      cache: GraphQLCache(),
+      link: link,
+    ),
+  );
+
+  // Future<String?> token() async => await AppSecureStorage.getToken();
 }
