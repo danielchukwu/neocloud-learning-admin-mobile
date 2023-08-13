83dbb24aba4c78583050849b72c4143243bc62ea
dc <00danzy@gmail.com>
2023-08-13 23:58:47 +0100

    implement: jwt expiration refetch token logic and add printouts for debugging

--- lib/utils/error_handler.dart
+++ lib/utils/error_handler.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:neocloud_mobile/app_secure_storage.dart';
 import 'package:neocloud_mobile/components/popups.dart';
+import 'package:neocloud_mobile/graphql/services/auth_service.dart';
 
-handleErrors(BuildContext context, QueryResult<Object?> result) {
+handleErrors(BuildContext context, QueryResult<Object?> result) async {
   List<GraphQLError>? graphqlErrors = result.exception?.graphqlErrors;
 
   // handle field errors
   if (graphqlErrors != null && graphqlErrors.length > 0) {
+    debugPrint('handleError: field errors 🧺');
     var errors = graphqlErrors[0].extensions?['errors'];
     var errorValues = errors.values.toList();
     showTopAlertDialog(context, text: errorValues.join(', '));
+    debugPrint('handleError: field errors ✅');
     return errorValues.join(', ');
   }
 
   // jwt expiration
-  print('JWT EXPIRATION');
   if ('$result'.contains('jwt expired')) {
-    showTopAlertDialog(context, text: 'jwt expired');
+    debugPrint('handleError: jwt expired - refreshing token 🔃');
+    await AppSecureStorage.deleteToken();
+    await AuthService().refreshToken(context);
+    debugPrint('handleError: jwt expired ✅');
     return 'jwt expired';
   }
 
+  debugPrint('handleError: Catch All ✅');
   showTopAlertDialog(context, text: 'An error occurred!');
-  return 'An error has occurred!';
+  return 'An error occurred!';
 }
