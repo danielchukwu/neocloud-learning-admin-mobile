57582663142cb48cbe0b47d20faec88588b8bfd1
dc <00danzy@gmail.com>
2023-08-14 22:43:03 +0100

    update: code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/providers/NavItem.dart';
+import 'package:neocloud_mobile/providers/UserProvider.dart';
+import 'package:neocloud_mobile/routes/app_router.dart';
 import 'package:neocloud_mobile/screens/academic/academic_screen.dart';
 import 'package:neocloud_mobile/screens/cart/cart_screen.dart';
 import 'package:neocloud_mobile/screens/comming_soon/comming_soon_screen.dart';
@@ -22,7 +24,8 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => NavItems()),
+        ChangeNotifierProvider<NavbarProvider>(create: (_) => NavbarProvider()),
+        ChangeNotifierProvider<UserProvider>(create: (_) => UserProvider()),
       ],
       child: const MyApp(),
     ),
@@ -33,38 +36,48 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     title: 'Neo Cloud Technologies',
+  //     theme: buildTheme(),
+  //     initialRoute: '/login',
+  //     routes: {
+  //       '/': (context) => const SplashScreen(),
+  //       '/login': (context) => const LoginSignupScreen(),
+  //       '/welcome': (context) => const WelcomeScreen(),
+  //       '/coming-soon': (context) => const ComingSoonScreen(),
+  //       '/academic': (context) => const AcademicScreen(),
+  //       '/dashboard': (context) => const DashboardScreen(),
+  //       // Removed because the ProfileScreen widget takes arguments
+  //       // '/profile': (context) => ProfileScreen(),
+  //       '/search': (context) => const SearchScreen(),
+  //       '/cart': (context) => const CartScreen(),
+  //       // Removed because the CourseScreen widget takes arguments
+  //       // '/course': (context) => const CourseScreen(),
+
+  //       // Settings
+  //       '/settings': (context) => const SettingsScreen(),
+  //       '/manage-account': (context) => const ManageAccountScreen(),
+  //       '/system': (context) => const SystemScreen(),
+  //       '/payment': (context) => const PaymentScreen(),
+  //       '/language': (context) => const LanguageScreen(),
+  //       '/smtp': (context) => SmtpScreen(),
+  //       '/general': (context) => SystemGeneralScreen(),
+  //       '/system-logos': (context) => SystemLogosScreen(),
+  //     },
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // SizeConfig().init(context);
-    return MaterialApp(
+    var appRouter = AppRouter();
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Neo Cloud Technologies',
+      title: 'NCT Pro',
       theme: buildTheme(),
-      initialRoute: '/login',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => const LoginSignupScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
-        '/coming-soon': (context) => const ComingSoonScreen(),
-        '/academic': (context) => const AcademicScreen(),
-        '/dashboard': (context) => const DashboardScreen(),
-        // Removed because the ProfileScreen widget takes arguments
-        // '/profile': (context) => ProfileScreen(),
-        '/search': (context) => const SearchScreen(),
-        '/cart': (context) => const CartScreen(),
-        // Removed because the CourseScreen widget takes arguments
-        // '/course': (context) => const CourseScreen(),
-
-        // Settings
-        '/settings': (context) => const SettingsScreen(),
-        '/manage-account': (context) => const ManageAccountScreen(),
-        '/system': (context) => const SystemScreen(),
-        '/payment': (context) => const PaymentScreen(),
-        '/language': (context) => const LanguageScreen(),
-        '/smtp': (context) => SmtpScreen(),
-        '/general': (context) => SystemGeneralScreen(),
-        '/system-logos': (context) => SystemLogosScreen(),
-      },
+      routerConfig: appRouter.config(),
     );
   }
 
