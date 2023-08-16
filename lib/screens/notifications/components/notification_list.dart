f99f9c44934abcb26262ce4b551438fc44c749dc
dc <00danzy@gmail.com>
2023-08-16 23:46:29 +0100

    first commit

---
+++ lib/screens/notifications/components/notification_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/constraints.dart';
+import 'package:neocloud_mobile/graphql/models/NotificationModel.dart';
+import 'package:neocloud_mobile/screens/notifications/components/notification-card.dart';
+
+class NotificationList extends StatelessWidget {
+  const NotificationList({super.key, required this.dataList});
+
+  final List<NotificationModel> dataList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(
+          dataList!.length,
+          (index) => NotificationCard(
+              notification: dataList[index], typeColor: kBlack70)),
+    );
+  }
+}
