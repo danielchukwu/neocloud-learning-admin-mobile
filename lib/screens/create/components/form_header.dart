a6b7843486f11b7d0aec21ae531b26c80b3a3802
dc <00danzy@gmail.com>
2023-08-20 19:38:34 +0100

    first commit

---
+++ lib/screens/create/components/form_header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/texts.dart';
+import '../../../constraints.dart';
+
+class FormHeader extends StatelessWidget {
+  const FormHeader({
+    super.key,
+    this.title = '',
+  });
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: defaultSize * 2, vertical: defaultSize * 1.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Title
+          TextExtraLarge(title: title, weight: FontWeight.w600, color: kBlack80),
+          // Cancel Button
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Icon(Icons.close, color: Colors.black87, size: defaultSize * 3,)),
+        ],
+      ),
+    );
+  }
+}
