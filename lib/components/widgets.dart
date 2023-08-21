34a275611db64f1b060068e3e505bad83fea5d2e
dc <00danzy@gmail.com>
2023-08-21 19:48:01 +0100

    update: add HorizontalRule and TextInputError

--- lib/components/widgets.dart
+++ lib/components/widgets.dart
@@ -30,3 +30,42 @@ Widget nothingWasFoundScreen({double? screenMaxHeight}){
     child: const Center(child: TextExtraLarge(title: 'Nothing was found', color: Colors.black38, weight: FontWeight.w600,)),
   );
 }
+
+// This is basically a horizontal rule or line that is gray in color
+class HorizontalRule extends StatelessWidget {
+  const HorizontalRule({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color ruleColor = kBlack.withOpacity(.2);
+
+    return Container(
+      height: defaultSize * .5,
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: ruleColor, width: 1)),
+      ),
+    );
+  }
+}
+
+// This is used for custom input fields in forms to display a simple text color in 
+// red to indicate an error (that may occur if a value for that field wasn't provided)
+class TextInputError extends StatelessWidget {
+  const TextInputError({
+    super.key,
+    this.text = 'This field is required'
+  });
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: defaultSize),
+        Text(text, style: TextStyle(color: Colors.red[800], fontSize: 12),),
+      ],
+    );
+  }
+}
