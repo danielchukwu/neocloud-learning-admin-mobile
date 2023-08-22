a78705a5245de083a666b57d125756d9acf55606
dc <00danzy@gmail.com>
2023-08-22 21:49:13 +0100

    update: add error handling

--- lib/screens/create/components/form_footer.dart
+++ lib/screens/create/components/form_footer.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 import '../../../components/texts.dart';
 import '../../../constraints.dart';
 
+/// This widget basically displays a button that when clicke should submit a form or carry out any action we want
+/// It takes a formKey and uses it to  check if the data in the form the formKey is attached to are all valid, 
+/// if they are it runs a function press( This should submit the form or perform some action)  
 class FormFooter extends StatelessWidget {
   const FormFooter({
     super.key, 
+    this.title = 'Create',
     required this.formKey,
     required this.press,
   });
-  final GlobalKey<FormState> formKey;
+  final String title;
+  final GlobalKey<FormState>? formKey;
   final Function() press;
 
   @override
@@ -31,12 +36,17 @@ class FormFooter extends StatelessWidget {
               padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: defaultSize * 2)),
             ),
             onPressed: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                press();
+              try {
+                if (formKey!.currentState!.validate()) {
+                  formKey!.currentState!.save();
+                  // submit form (or do something)
+                  press();
+                }
+              } catch (e) {
+                Navigator.pop(context);
               }
             },
-            child: const TextSmall(title: 'Create', weight: FontWeight.w600, color: Colors.black54,),
+            child: TextSmall(title: title, weight: FontWeight.w600, color: Colors.black54,),
           )
         ],
       ),
