7838651d48292d98328997795456dbe48931de2e
dc <00danzy@gmail.com>
2023-08-25 22:47:40 +0100

    complete first implementation

--- lib/screens/create/components/form_module_tile.dart
+++ lib/screens/create/components/form_module_tile.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:neocloud_mobile/components/input/input_fields.dart';
 import 'package:neocloud_mobile/components/popups/popups.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
-import 'package:neocloud_mobile/size_config.dart';
-import 'package:neocloud_mobile/utils/validation.dart';
+import 'package:neocloud_mobile/screens/create/components/form_update_or_delete_inputfield.dart';
 
 // This is a form class module tile widget that shows a modules title and it's order count
 // and then it shows the number of schedules the module has and also an add schedules button
@@ -28,13 +26,13 @@ class FormModuleTile extends StatefulWidget {
 }
 
 class _FormModuleTileState extends State<FormModuleTile> {
-  final _controller = TextEditingController();
+  late ClassModuleModel _module;
   bool _editMode = false;
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _module = widget.module;
   }
 
   @override
@@ -60,20 +58,16 @@ class _FormModuleTileState extends State<FormModuleTile> {
     );
   }
 
-  updateEditMode(bool val) {
-    setState(() { _editMode = val; });
-  }
-
   Widget buildBody() {
     return GestureDetector(
-      onTap: () => updateEditMode(true),
+      onTap: () => setState(() { _editMode = true; }),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // title
           _editMode == false 
-          ? TextMedium(  title: widget.module.title, color: kBlack80, weight: FontWeight.w500)
-          : buildUDModuleInputField(),
+          ? TextMedium(  title: _module.title, color: kBlack80, weight: FontWeight.w500)
+          : FormUpdateOrDeleteInputField(fontSize: defaultSize * 1.6, textColor: kBlack80, fontWeight: FontWeight.w500, hintText: 'Schedule Title', initialValue: _module.title, pressUpdate: updateModule, pressDelete: () => widget.pressDeleteModule(_module)),
     
           // row - schedules count, view schedules
           SizedBox(height: defaultSize * .8),
@@ -82,14 +76,14 @@ class _FormModuleTileState extends State<FormModuleTile> {
             children: [
               // schedules count
               TextSmall(
-                title: '${widget.module.classSchedules?.length ?? 0} Schedules',
+                title: '${_module.classSchedules?.length ?? 0} Schedules',
                 color: Colors.black54,
               ),
     
               // view schedules
               GestureDetector(
-                onTap: () => showCreateScheduleDialog(module: widget.module, moduleCount: widget.count, updateModule: widget.pressUpdateModule),
-                child: TextSmall(
+                onTap: () => showCreateScheduleDialog(module: _module, moduleCount: widget.count, updateModule: widget.pressUpdateModule),
+                child: const TextSmall(
                   title: 'View Schedules',
                   color: Colors.black54,
                 ),
@@ -101,58 +95,11 @@ class _FormModuleTileState extends State<FormModuleTile> {
     );
   }
 
-
-  Row buildUDModuleInputField() {
-    return Row(
-      children: [
-        // Textarea - Edit Title
-        Expanded(
-          child: Container(
-            // color: kRed,
-            height: defaultSize * 2.4,
-            child: Stack(
-              children: [
-                Positioned(
-                  height: defaultSize * 5,
-                  width: SizeConfig.screenWidth! / 1.4,
-                  top: -10,
-                  child: Container(
-                    // color: kGreen,
-                    child: FormTextarea(
-                      initialValue: widget.module.title,
-                      controller: _controller,
-                      maxLines: 1,
-                      validator: validateRequireField, 
-                      press: (_) {}, 
-                      autoFocus: _editMode,
-                      pressOnKeyboardDone: updateModule,
-                      onTapOutside: (_) {
-                        // We delay the call of this just incase the user tapped on the delete icon, 
-                        // this wouldn't prevent the delete action
-                        Future.delayed(const Duration(milliseconds: 300), updateModule);
-                      },
-                    ),
-                  ),
-                ),
-              ]
-            ),
-          ),
-        ),
-
-        // Icon
-        GestureDetector(
-          onTap: () {
-            print('DELETE ICON TAPPED');
-            widget.pressDeleteModule(widget.module);
-          },
-          child: Icon(Icons.delete_outline, color: kRed.withOpacity(.9), size: defaultSize * 2)
-        ),
-      ],
-    );
-  }
-
-  updateModule() {
-    widget.pressUpdateModule(widget.module, ClassModuleModel.fromInstance(module: widget.module, title: _controller.text));
-    updateEditMode(false);
+  updateModule(String title) {
+    // widget.pressUpdateModule(widget.module, ClassModuleModel.fromInstance(module: widget.module, title: title));
+    setState(() => _editMode = false );
+    if (_module.title != title) {
+      setState(() { _module = ClassModuleModel(id: _module.id, title: title); });
+    }
   }
 }
