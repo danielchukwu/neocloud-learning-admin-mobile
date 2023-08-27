8e97d3315977079fece0638ff371b5d3abd7f754
dc <00danzy@gmail.com>
2023-08-27 23:39:01 +0100

    improve code - milestone 🦜

--- lib/screens/create/components/form_schedule_tile.dart
+++ lib/screens/create/components/form_schedule_tile.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:neocloud_mobile/components/popups/popups.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/graphql/models/ClassScheduleModel.dart';
 import 'package:neocloud_mobile/screens/create/components/form_update_or_delete_inputfield.dart';
 
-// This is a form class module tile widget that shows a modules title and it's order count
+// This is a form class module tile widget that shows a modules title and it's order Index
 // and then it shows the number of schedules the module has and also an add schedules button
 class FormScheduleTile extends StatefulWidget {
   const FormScheduleTile({
     super.key,
     required this.schedule,
-    required this.count,
+    required this.index,
     required this.pressUpdateSchedule,
     required this.pressDeleteSchedule,
   });
 
   final ClassScheduleModel schedule;
-  final int count;
-  final Function(ClassScheduleModel schedule, ClassScheduleModel newSchedule) pressUpdateSchedule;
-  final Function(ClassScheduleModel module) pressDeleteSchedule;
+  final int index;
+  final Function(int scheduleIndex, ClassScheduleModel newSchedule) pressUpdateSchedule;
+  final Function(int scheduleIndex) pressDeleteSchedule;
 
   @override
   State<FormScheduleTile> createState() => _FormScheduleTileState();
@@ -42,16 +43,16 @@ class _FormScheduleTileState extends State<FormScheduleTile> {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Count
+          // Index
           SizedBox(width: defaultSize),
           SizedBox(
             width: defaultSize * 3, 
             child: _editTitleMode == false
-            ? TextMedium(title: '${widget.count}', color: Colors.black54)
-            : SizedBox(),
+            ? TextMedium(title: '${widget.index + 1}', color: Colors.black54)
+            : const SizedBox(),
           ),
 
-          // Column - Title, Row (schedules count, add schedules btn)
+          // Column - Title, Row (schedules Index, add schedules btn)
           Expanded(child: buildBody())
         ],
       ),
@@ -80,7 +81,7 @@ class _FormScheduleTileState extends State<FormScheduleTile> {
                   child: TextSmall(title: _schedule.description ?? ''),
                 ),
             )
-          : SizedBox()
+          : const SizedBox()
         : FormUpdateOrDeleteInputField(fontSize: defaultSize * 1.4, textColor: kBlack70, hintText: 'Description', initialValue: _schedule.description, pressUpdate: updateScheduleDescription),
 
     
@@ -97,8 +98,15 @@ class _FormScheduleTileState extends State<FormScheduleTile> {
             : const SizedBox(),
     
             // add classwork btn
-            _schedule.description == null ? SizedBox(width: defaultSize * 2) : const SizedBox(),
-            const TextSmall(title: 'Add Classwork', color: Colors.black54),
+            _schedule.description == null && _editDescriptionMode == false ? SizedBox(width: defaultSize * 2) : const SizedBox(),
+            GestureDetector(
+              onTap: () => showCreateClassworkDialog(schedule: _schedule, index: widget.index, updateSchedule: (scheduleIndex, newSchedule) {
+                // update this tile
+                setState(() {_schedule = newSchedule;});
+                // update parent 
+                widget.pressUpdateSchedule(scheduleIndex, newSchedule);
+              },),
+              child: TextSmall(title: _schedule.classwork == null ? 'Add Classwork' : 'View Classwork', color: Colors.black54)),
           ],
         )
       ],
@@ -109,21 +117,18 @@ class _FormScheduleTileState extends State<FormScheduleTile> {
     setState(() => _editTitleMode = false );
     if (title.isNotEmpty && title != widget.schedule.title) {
       setState(() { _schedule = ClassScheduleModel.fromInstance(cs: _schedule, title: title); });
-      // update the parent screen state
-      widget.pressUpdateSchedule(_schedule,  ClassScheduleModel.fromInstance(cs: _schedule, title: title));
     }
   }
 
   updateScheduleDescription(String description) {
-    if (description.isNotEmpty) {
-      setState(() {  _schedule = ClassScheduleModel.fromInstance(cs: _schedule, description: description);  _editDescriptionMode = false; });
-      widget.pressUpdateSchedule(_schedule,  ClassScheduleModel.fromInstance(cs: _schedule, description: description));
+    setState(() { _editDescriptionMode = false; });
+    if (description.isNotEmpty && description != _schedule.description) {
+      setState(() { _schedule = ClassScheduleModel.fromInstance(cs: _schedule, description: description); });
+      widget.pressUpdateSchedule(widget.index,  ClassScheduleModel.fromInstance(cs: _schedule, description: description));
     }
   }
 
   deleteSchedule() {
-    // delete schedule
-    debugPrint('FormScheduleTile: DELETING 1');
-    widget.pressDeleteSchedule(_schedule);
+    widget.pressDeleteSchedule(widget.index);
   }
 }
