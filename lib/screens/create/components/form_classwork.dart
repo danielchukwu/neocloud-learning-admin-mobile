789ad6b14df0a5f4daae198d69c0d9c4c115be72
dc <00danzy@gmail.com>
2023-08-27 23:35:11 +0100

    update: complete v1

--- lib/screens/create/components/form_classwork.dart
+++ lib/screens/create/components/form_classwork.dart
@@ -12,8 +12,6 @@ import 'package:neocloud_mobile/screens/create/components/form_header.dart';
 import 'package:neocloud_mobile/screens/create/components/from_index_title_header.dart';
 import 'package:neocloud_mobile/utils/validation.dart';
 
-
-
 /// POPUP
 /// This screen is used in a showDialog dialog context, so it basically will be shown in a pop up context 
 /// called for now by the FormScheduleTile() widget in the CreateClassworkScreen() for adding classworks to schedules that are created for a class
@@ -31,25 +29,45 @@ class FormClassworkScreen extends StatefulWidget {
 class _FormClassworkScreenState extends State<FormClassworkScreen> {
   final _formKey = GlobalKey<FormState>();
   late ClassScheduleModel _schedule;
-  late ClassworkModel _classworkModel;
-  final _durationOptionsData = {'1d': const Duration(days:1), '2d': const Duration(days: 2), '3d': const Duration(days: 3), '5d': const Duration(days: 5), '1w': const Duration(days: 7)};
+  late ClassworkModel? _classwork;
+  final _durationOptionsData = {'1d': 1, '2d': 2, '3d': 3, '5d': 5, '1w': 7};
+  late List<String> _durations;  // list of keys from _durationOptionsData e.x ['1d', '2d', '3d', ...]
 
   // classwork field values
-  String title = '';
-  String description = '';
+  var titleController = TextEditingController();
+  var descriptionController = TextEditingController();
   int _selectedDuration = 4;
 
   @override
   void initState() {
     super.initState();
+    loadData();
+  }
+
+  loadData() {
     _schedule = widget.schedule;
-    _classworkModel = widget.schedule.classwork ?? ClassworkModel(id: '', title: '', body: '');
+    _classwork = widget.schedule.classwork;
+    _durations = _durationOptionsData.keys.toList();
+    titleController.text = _schedule.classwork?.title ?? '';
+    descriptionController.text = _schedule.classwork?.body ?? '';
+    debugPrint(descriptionController.text);
+  }
+
+  submitForm() {
+    debugPrint(titleController.text);
+    debugPrint(descriptionController.text);
+    debugPrint('${_durationOptionsData[ _durations[_selectedDuration] ]}');
+
+    int? days = _durationOptionsData[ _durations[_selectedDuration] ];
+    var newClasswork = ClassworkModel(id: '1', title: titleController.text, body: descriptionController.text, duration: days ?? 7);
+    var newClassSchedule = ClassScheduleModel.fromInstance(cs: widget.schedule, classwork: newClasswork);
+    print(newClassSchedule.classwork!.body);
+    widget.updateSchedule(widget.index, newClassSchedule);
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> durations = _durationOptionsData.keys.toList();
-
     return Container(
       margin: EdgeInsets.symmetric(horizontal: defaultSize , vertical: defaultSize * 4),
       child: Scaffold(
@@ -78,12 +96,12 @@ class _FormClassworkScreenState extends State<FormClassworkScreen> {
                 Expanded(
                   child: SingleChildScrollView(
                     padding: screenPadding,
-                    child: buildForm(durations),
+                    child: buildForm(_durations),
                   ),
                 ),
           
                 const HorizontalRule(),
-                FormFooter(title: 'Save', formKey: _formKey, press: () => Navigator.pop(context))
+                FormFooter(title: 'Save', formKey: _formKey, press: submitForm)
               ],
             ),
           ),
@@ -92,57 +110,64 @@ class _FormClassworkScreenState extends State<FormClassworkScreen> {
     );
   }
 
+  // Column - classwork  title, description, duration, attach file
   Column buildForm(List<String> durations) {
     return Column(
       children: [
         // Classwork Title Input Field
         SizedBox(height: defaultSize * 2),
         FormInputField(
+          controller: titleController,
           hintText: 'Classwork Title', fontSize: defaultSize * 1.8, fontWeight: FontWeight.w500,
           validator: validateRequireField,
-          press: (value) {},
+          press: (_) {},
         ),
 
         // Description
-        FormDescription(maxLine: 5, press: (v) {}),
+        FormDescription(controller: descriptionController, maxLine: 5, press: (_) {},),
 
         SizedBox(height: defaultSize * 2),
         const HorizontalRule(),
 
-        // Duration Selection
+        // Duration Section - Row (iconText, duration options)
         SizedBox(height: defaultSize* 2),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconText(title: 'Duration', icon: Icons.access_time_outlined, color: kBlack70, iconColor: kBlack70),
-
-            SizedBox(width: defaultSize * 2),
-            Expanded(
-              child: Wrap(
-                runSpacing: defaultSize,
-                spacing: defaultSize,
-                children: List.generate(
-                  durations.length, 
-                  (index) => GestureDetector(
-                    onTap: () => setState(() => _selectedDuration = index ),
-                    child: AvatarInsertWidget(widget: TextMedium(title: durations[index], color: _selectedDuration == index ? Colors.white : Colors.black54), backgroundColor: _selectedDuration == index ? kBlueLight : Colors.black12)),
-                ),
-              ),
-            )
-          ],
-        ),
-
+        buildDuration(),
 
         SizedBox(height: defaultSize * 2),
         const HorizontalRule(),
 
+        // Attach file
         SizedBox(height: defaultSize * 2),
         IconText(title: 'Attach file', icon: Icons.attach_file_rounded, iconColor: kBlack70,),
 
+        SizedBox(height: defaultSize * 5),
       ],
     );
   }
 
+  Row buildDuration() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconText(title: 'Duration', icon: Icons.access_time_outlined, color: kBlack70, iconColor: kBlack70),
+
+          SizedBox(width: defaultSize * 2),
+          Expanded(
+            child: Wrap(
+              runSpacing: defaultSize,
+              spacing: defaultSize,
+              children: List.generate(
+                _durations.length, 
+                (index) => GestureDetector(
+                  onTap: () => setState(() => _selectedDuration = index ),
+                  child: AvatarInsertWidget(widget: TextMedium(title: _durations[index], color: _selectedDuration == index ? Colors.white : Colors.black54), backgroundColor: _selectedDuration == index ? kBlueLight : Colors.black12)),
+              ),
+            ),
+          )
+        ],
+      );
+  }
+  
   Widget buildScheduleListHeader() {
     return IconText(
       title: 'Schedules',
@@ -155,5 +180,3 @@ class _FormClassworkScreenState extends State<FormClassworkScreen> {
     );
   }
 }
-
-
