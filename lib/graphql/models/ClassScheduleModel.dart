5ed974f967438ff69060ad0423e29b76ea6177be
dc <00danzy@gmail.com>
2023-08-27 23:38:19 +0100

    update: add classwork field

--- lib/graphql/models/ClassScheduleModel.dart
+++ lib/graphql/models/ClassScheduleModel.dart
@@ -1,4 +1,5 @@
 import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
+import 'package:neocloud_mobile/graphql/models/ClassworkModel.dart';
 
 class ClassScheduleModel {
   final String id;
@@ -8,6 +9,7 @@ class ClassScheduleModel {
   final int? order;
   final String? startTime;
   final String? endTime;
+  final ClassworkModel? classwork;
   final ClassModuleModel? classModule;
 
 
@@ -19,6 +21,7 @@ class ClassScheduleModel {
     this.order,
     this.startTime,
     this.endTime,
+    this.classwork,
     this.classModule
   });
 
@@ -30,10 +33,11 @@ class ClassScheduleModel {
     order: cs['order'],
     startTime: cs['startTime'],
     endTime: cs['endTime'],
+    classwork: cs.containsKey('classwork') ? ClassworkModel.fromMap(cw: cs['classwork']) : null,
     classModule: cs.containsKey('classModule') ? ClassModuleModel.fromMap(classModule: cs['classModule']) : null,
   );
 
-  static ClassScheduleModel fromInstance({required ClassScheduleModel cs, String? title, String? description}) => ClassScheduleModel(
+  static ClassScheduleModel fromInstance({required ClassScheduleModel cs, String? title, String? description, ClassworkModel? classwork}) => ClassScheduleModel(
     id: cs.id,
     title: title ?? cs.title,
     description: description ?? cs.description,
@@ -41,6 +45,7 @@ class ClassScheduleModel {
     order: cs.order,
     startTime: cs.startTime,
     endTime: cs.endTime,
+    classwork: classwork ?? cs.classwork,
     classModule: cs.classModule
   );
 }
