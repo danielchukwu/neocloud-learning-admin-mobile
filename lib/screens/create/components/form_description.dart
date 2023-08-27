6f8577b19242214f81d617bb27cd7e744853ca61
dc <00danzy@gmail.com>
2023-08-27 23:34:41 +0100

    update: add controller field

--- lib/screens/create/components/form_description.dart
+++ lib/screens/create/components/form_description.dart
@@ -7,10 +7,14 @@ import 'package:neocloud_mobile/utils/validation.dart';
 class FormDescription extends StatelessWidget {
   const FormDescription({
     super.key,
+    this.maxLine,
+    this.controller,
     required this.press
   });
 
   final Function(String? value) press;
+  final int? maxLine;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class FormDescription extends StatelessWidget {
       children: [
         // Icon
         Padding(
-          padding: EdgeInsets.only(top: defaultSize * 1.3),
+          padding: EdgeInsets.only(top: defaultSize * 1.2),
           child: Icon(Icons.short_text, color: Colors.black54),
         ),
       
@@ -28,6 +32,8 @@ class FormDescription extends StatelessWidget {
         Container(
           width: SizeConfig.screenWidth! / 1.3,
           child: FormTextarea(
+            controller: controller,
+            maxLines: maxLine ?? 10,
             hintText: 'Add Description',
             validator: validateRequireField,
             press: press,
