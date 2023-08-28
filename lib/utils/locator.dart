a689764432bd3af2bdca24bab8ea1257050f4cce
dc <00danzy@gmail.com>
2023-08-28 22:31:12 +0100

    first commit

---
+++ lib/utils/locator.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'package:neocloud_mobile/utils/flutter_storage.dart';
+import 'package:neocloud_mobile/utils/image_helper.dart';
+
+GetIt getIt = GetIt.I;
+
+void setupServiceLocator() {
+  // ImageHelper : allows us pick images from the platform gallery or camera
+  getIt.registerLazySingleton<ImageHelper>(() => ImageHelper());
+  // FirebaseStorage : allows us store images on the cloud
+  getIt.registerLazySingleton<FirebaseStorage>(() => FirebaseStorage());
+}
