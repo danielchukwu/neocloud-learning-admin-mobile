04a16edd32c062a2c62e3578a3beaa7053746a8f
dc <00danzy@gmail.com>
2023-09-02 23:15:30 +0100

    update: replace defaultSize with const integers

--- lib/screens/create/components/form_header.dart
+++ lib/screens/create/components/form_header.dart
@@ -13,7 +13,7 @@ class FormHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: defaultSize * 2, vertical: defaultSize * 1.5),
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -22,7 +22,7 @@ class FormHeader extends StatelessWidget {
           // Cancel Button
           GestureDetector(
             onTap: () => Navigator.pop(context),
-            child: Icon(Icons.close, color: Colors.black87, size: defaultSize * 3,)),
+            child: Icon(Icons.close, color: Colors.black87, size: 30,)),
         ],
       ),
     );
