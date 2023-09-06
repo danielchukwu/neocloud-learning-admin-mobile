0fb91d915be5009cb75f9d663de41ab671552129
dc <00danzy@gmail.com>
2023-09-06 21:26:13 +0100

    update: complete image upload to cloud and form features

--- lib/screens/create/create_class_screen.dart
+++ lib/screens/create/create_class_screen.dart
@@ -1,22 +1,14 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:neocloud_mobile/components/input/input_fields.dart';
-import 'package:neocloud_mobile/components/popups/popups.dart';
-import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/components/widgets.dart';
 import 'package:neocloud_mobile/constraints.dart';
 import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
-import 'package:neocloud_mobile/graphql/models/ClassScheduleModel.dart';
-import 'package:neocloud_mobile/graphql/models/ClassworkModel.dart';
 import 'package:neocloud_mobile/graphql/models/FacultyModel.dart';
 import 'package:neocloud_mobile/graphql/models/UserModel.dart';
+import 'package:neocloud_mobile/screens/create/components/form_add_cover.dart';
 import 'package:neocloud_mobile/screens/create/components/form_modules.dart';
 import 'package:neocloud_mobile/screens/create/components/form_select_faculty.dart';
-import 'package:neocloud_mobile/utils/flutter_storage.dart';
-import 'package:neocloud_mobile/utils/image_helper.dart';
-import 'package:neocloud_mobile/utils/locator.dart';
 import 'package:neocloud_mobile/utils/validation.dart';
 import 'components/form_description.dart';
 import 'components/form_footer.dart';
@@ -38,42 +30,76 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
   final _formKey = GlobalKey<FormState>();
   final _scrollController = ScrollController();
 
-  String? _titleField;
-  String? _descriptionField;
-  List<FacultyModel> _selectedFaculties = [];  // we'll only use a single Faculty, _selectedFaculties[0] 
+  // Mock Data
   final List<FacultyModel> _facultiesToSelectFrom = List.generate(15, (index) => FacultyModel(id: '$index', name: 'Cyber Security'));
-
-  List<UserModel> _selectedEducatorsList = [];
   final List<UserModel> _usersToSelectFrom = List.generate(15, (index) => UserModel(id: '$index', name: 'John Default', avatar: defaultProfileAvatar));
-  // Modules
-  final List<ClassModuleModel> _modules = [ClassModuleModel(id: '1', title: 'Introduction to Tech', classSchedules: [ClassScheduleModel(id: '1', title: 'What is Tech', classwork: ClassworkModel(id: '1', title: 'Binary Sort Tree', body: 'just do it', duration: 2))])];
 
-  // handle _hod errors and _educatorsList errors
+  File? _coverImgFile;    // class image file
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  List<FacultyModel> _selectedFaculties = [];  // we'll only use a single Faculty, _selectedFaculties[0] 
+  List<UserModel> _selectedEducatorsList = [];
+  final List<ClassModuleModel> _modules = [];
+
+  // error handling variables
+  bool _coverImgFileHasError = false;
   bool _hodHasError = false;
   bool _educatorsListHasError = false;
+  bool _modulesHasError = false;
+  String _modulesErrorMessage = '';
 
-  void submitForm() {
-    // confirm _hod && _educatorsList are not null 
+  bool isSubmittingForm = false;
+
+  updateClassCoverImage(File file) {
+    setState(() { _coverImgFile = file; });
+  }
+
+  bool formDataIsValid() {
+    // cover must be provided
+    if (_coverImgFile == null) {
+      setState(() { _coverImgFileHasError = true; });
+      return false;
+    }
+    setState(() { _coverImgFileHasError = false; });
+
+    // a faculty must be provided
     if (_selectedFaculties.isEmpty) {
       setState(() { _hodHasError = true; });
-      return;
+      return false;
     }
     setState(() { _hodHasError = false; });
 
+    // atleast 1 educator must be provided
     if (_selectedEducatorsList.isEmpty) {
       setState(() { _educatorsListHasError = true; });
-      return;
+      return false;
     }
     setState(() { _educatorsListHasError = false; });
 
-    debugPrint(_titleField);
-    debugPrint(_descriptionField);
-    debugPrint('$_selectedFaculties');
-    debugPrint('$_selectedEducatorsList');
+    // modules must be provided (at least 3)
+    if (_modules.isEmpty || _modules.length < 3) {
+      // showTopAlertDialog(text: 'At least 3 modules must be added');
+      setState(() { _modulesHasError = true; _modulesErrorMessage = 'At least 3 modules must be added'; });
+      return false;
+    } 
+    setState(() { _modulesHasError = false; _modulesErrorMessage = ''; });
+
+    // All modules must have at least 1 schedule in them
+    for (var module in _modules) {
+      if (module.classSchedules == null || module.classSchedules != null && module.classSchedules!.length == 0) {
+        // showTopAlertDialog(text: 'All specified modules should have at least 1 schedule added');
+        setState(() { _modulesHasError = true; _modulesErrorMessage = 'All specified modules should have at least 1 schedule added'; });
+        return false;
+      }
+    }
+    setState(() { _modulesHasError = false; _modulesErrorMessage = ''; });
+
+    return true;
   }
 
-  updateClassName(value) { _titleField = value; }
-  updateDescription(value) { _descriptionField = value; }
+  Future<void> submitForm() async {
+    await Future.delayed(const Duration(seconds: 5));
+  }
 
   updateSelectedEducators(List<UserModel> users) {
     setState(() { _selectedEducatorsList = users; });
@@ -85,8 +111,10 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
 
   @override
   void dispose() {
-    _scrollController.dispose();
     super.dispose();
+    _scrollController.dispose();
+    _titleController.dispose();
+    _descriptionController.dispose();
   }
 
   @override
@@ -96,7 +124,7 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
       body: Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(defaultSize * 2)
+          borderRadius: BorderRadius.circular(20)
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -116,7 +144,22 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
             ),
 
             const HorizontalRule(),
-            FormFooter(formKey: _formKey, press: submitForm),
+            FormFooter(
+              formKey: _formKey, 
+              btnIsLoading: isSubmittingForm,
+              press: () async {
+                setState(() { isSubmittingForm = true; });
+                // submit form if form data is valid
+                print('1');
+                if (formDataIsValid()) {
+                  print('2');
+                  // submit form
+                  await submitForm();
+                }
+                print('3');
+                setState(() { isSubmittingForm = false; });
+              }
+            ),
           ],
         ),
       
@@ -131,107 +174,59 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          SizedBox(height: defaultSize * 3),
+          const SizedBox(height: 30),
 
           // Class Avatar / Cover
-          FormAddCover(),
+          FormAddCover(press: updateClassCoverImage),
+          _coverImgFileHasError ? const TextInputError(text: 'A class requires an image cover') : const SizedBox(),
           
           // Class Name
-          SizedBox(height: defaultSize * 3),
+          const SizedBox(height: 30),
           FormInputField(
-            hintText: 'Class Name', fontSize: defaultSize * 2, fontWeight: FontWeight.w500,
-            validator: validateRequireField,
-            press: updateClassName,
+            controller: _titleController,
+            hintText: 'Class Name', 
+            fontSize: 20, 
+            fontWeight: FontWeight.w500,
+            validator: validateRequireField, 
+            press: (_) {},
           ),
-  
-          // Add Faculty
-          SizedBox(height: defaultSize * 1.5 ),
-          FormSelectFaculty(buttonText: 'Faculty', selectedFacultyList: _selectedFaculties, facultiesToSelectFrom: _facultiesToSelectFrom, updateSelectedFaculty: updateSelectedFaculty),
           _hodHasError ? const TextInputError() : const SizedBox(),
 
-          SizedBox(height: defaultSize * 2 ),
+  
+          // Add Faculty
+          const SizedBox(height: 15),
+          FormSelectFaculty(buttonText: 'Faculty', selectedFacultyList: _selectedFaculties, facultiesToSelectFrom: _facultiesToSelectFrom, updateSelectedFaculty: updateSelectedFaculty),
+          _hodHasError ? const TextInputError() : const SizedBox(),
+          const SizedBox(height: 15),
+
           const HorizontalRule(),
 
           // Description
-          SizedBox(height: defaultSize * .8 ),
-          FormDescription(press: updateDescription),
+          const SizedBox(height: 8),
+          FormDescription(controller: _descriptionController, press: (_) {}),
+          const SizedBox(height: 5),
 
-          SizedBox(height: defaultSize * .5),
           const HorizontalRule(),
 
           // Add Educators
-          SizedBox(height: defaultSize * 1.5 ),
+          const SizedBox(height: 15),
           FormSelectUsers(buttonText: 'Educators', avatarText: 'ED', selectedUsersList: _selectedEducatorsList, usersToSelectFrom: _usersToSelectFrom, updateSelectedUsers: updateSelectedEducators,),
           _educatorsListHasError ? const TextInputError() : const SizedBox(),
+          const SizedBox(height: 15),
 
-          // Add Modules
-          SizedBox(height: defaultSize * 1.3 ),
           const HorizontalRule(),
 
-          SizedBox(height: defaultSize * 2 ),
+          // Add Modules
+          const SizedBox(height: 15),
+          _modulesHasError 
+          ? Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: TextInputError(text: _modulesErrorMessage),
+          ) : const SizedBox(),
           FormModules(modules: _modules, scrollController: _scrollController),
+
         ],
       )
-    );
-  }
-}
-
-// final imageHelper = ImageHelper();
-
-class FormAddCover extends StatefulWidget {
-  const FormAddCover({
-    super.key,
-  });
-
-  @override
-  State<FormAddCover> createState() => _FormAddCoverState();
-}
-
-class _FormAddCoverState extends State<FormAddCover> {
-  File? _selectedImage;
-  var imageHelper = getIt<ImageHelper>();
-  var firebaseStorage = getIt<FirebaseStorage>();
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        final files = await imageHelper.pickImage(); 
-        if (files.isNotEmpty) {
-          final croppedFile = await imageHelper.cropImage(file: files.first!, aspectRatio: const CropAspectRatio(ratioX: 4, ratioY: 2));
-          if (croppedFile != null) { 
-            setState(() => _selectedImage = File(croppedFile.path));
-            print(File(croppedFile.path));
-            debugPrint('UPLOADING IMAGE ...................... 👟👟👟');
-            print('FILENAME: ${files.first!.name}');
-            firebaseStorage.uploadFile(File(croppedFile.path), files.first!.name)
-            .then((value) => {debugPrint('DONE UPLOADING ........ ') });
-          }
-        }
-      },
-      child: AnimatedContainer(
-        duration: const Duration(seconds: 2),
-        height: defaultSize * 20,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          // borderRadius: BorderRadius.all(Radius.circular(defaultSize * .5)),
-          image: _selectedImage != null ? DecorationImage(image: FileImage(_selectedImage!), fit: BoxFit.cover) : null,
-        ),
-        child: _selectedImage == null ? Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Image
-              SizedBox(height: defaultSize * 1.5),
-              Icon(Icons.image, color: Colors.black38, size: defaultSize * 4),
-              // Text
-              SizedBox(height: defaultSize * .5),
-              const TextMedium(title: 'Add Cover', weight: FontWeight.w600, color: Colors.black26),
-            ],
-          ),
-        ) : const SizedBox(),
-      ),
     );
   }
 }
