e764ab2d1e34d7c94615399085f2ee5dffb73238
dc <00danzy@gmail.com>
2023-09-08 21:16:55 -1100

    update: use GetX state management

--- lib/screens/create/create_class_screen.dart
+++ lib/screens/create/create_class_screen.dart
@@ -1,121 +1,28 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:neocloud_mobile/components/input/input_fields.dart';
 import 'package:neocloud_mobile/components/widgets.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
-import 'package:neocloud_mobile/graphql/models/FacultyModel.dart';
-import 'package:neocloud_mobile/graphql/models/UserModel.dart';
 import 'package:neocloud_mobile/screens/create/components/form_add_cover.dart';
 import 'package:neocloud_mobile/screens/create/components/form_modules.dart';
 import 'package:neocloud_mobile/screens/create/components/form_select_faculty.dart';
+import 'package:neocloud_mobile/screens/create/controllers/create_class_controller.dart';
 import 'package:neocloud_mobile/utils/validation.dart';
 import 'components/form_description.dart';
 import 'components/form_footer.dart';
 import 'components/form_header.dart';
 import 'components/form_select_users.dart';
 
+
 /// POPUP
 /// This Screen is used in a showDialog, so it will basically not have 
 /// its own screen per say but will be used in a pop up environment instead
-class CreateClassScreen extends StatefulWidget {
-  const CreateClassScreen({super.key});
+class CreateClassScreen extends StatelessWidget {
+  CreateClassScreen({super.key});
+
   static String screenName = 'create class';
-
-  @override
-  State<CreateClassScreen> createState() => _CreateClassScreenState();
-}
-
-class _CreateClassScreenState extends State<CreateClassScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final _scrollController = ScrollController();
-
-  // Mock Data
-  final List<FacultyModel> _facultiesToSelectFrom = List.generate(15, (index) => FacultyModel(id: '$index', name: 'Cyber Security'));
-  final List<UserModel> _usersToSelectFrom = List.generate(15, (index) => UserModel(id: '$index', name: 'John Default', avatar: defaultProfileAvatar));
-
-  File? _coverImgFile;    // class image file
-  final _titleController = TextEditingController();
-  final _descriptionController = TextEditingController();
-  List<FacultyModel> _selectedFaculties = [];  // we'll only use a single Faculty, _selectedFaculties[0] 
-  List<UserModel> _selectedEducatorsList = [];
-  final List<ClassModuleModel> _modules = [];
-
-  // error handling variables
-  bool _coverImgFileHasError = false;
-  bool _hodHasError = false;
-  bool _educatorsListHasError = false;
-  bool _modulesHasError = false;
-  String _modulesErrorMessage = '';
-
-  bool isSubmittingForm = false;
-
-  updateClassCoverImage(File file) {
-    setState(() { _coverImgFile = file; });
-  }
-
-  bool formDataIsValid() {
-    // cover must be provided
-    if (_coverImgFile == null) {
-      setState(() { _coverImgFileHasError = true; });
-      return false;
-    }
-    setState(() { _coverImgFileHasError = false; });
-
-    // a faculty must be provided
-    if (_selectedFaculties.isEmpty) {
-      setState(() { _hodHasError = true; });
-      return false;
-    }
-    setState(() { _hodHasError = false; });
-
-    // atleast 1 educator must be provided
-    if (_selectedEducatorsList.isEmpty) {
-      setState(() { _educatorsListHasError = true; });
-      return false;
-    }
-    setState(() { _educatorsListHasError = false; });
-
-    // modules must be provided (at least 3)
-    if (_modules.isEmpty || _modules.length < 3) {
-      // showTopAlertDialog(text: 'At least 3 modules must be added');
-      setState(() { _modulesHasError = true; _modulesErrorMessage = 'At least 3 modules must be added'; });
-      return false;
-    } 
-    setState(() { _modulesHasError = false; _modulesErrorMessage = ''; });
-
-    // All modules must have at least 1 schedule in them
-    for (var module in _modules) {
-      if (module.classSchedules == null || module.classSchedules != null && module.classSchedules!.length == 0) {
-        // showTopAlertDialog(text: 'All specified modules should have at least 1 schedule added');
-        setState(() { _modulesHasError = true; _modulesErrorMessage = 'All specified modules should have at least 1 schedule added'; });
-        return false;
-      }
-    }
-    setState(() { _modulesHasError = false; _modulesErrorMessage = ''; });
-
-    return true;
-  }
-
-  Future<void> submitForm() async {
-    await Future.delayed(const Duration(seconds: 5));
-  }
-
-  updateSelectedEducators(List<UserModel> users) {
-    setState(() { _selectedEducatorsList = users; });
-  }
-
-  updateSelectedFaculty(List<FacultyModel> fac) {
-    setState(() { _selectedFaculties = fac; });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _scrollController.dispose();
-    _titleController.dispose();
-    _descriptionController.dispose();
-  }
+  var c = Get.put(ClassGetXController());
 
   @override
   Widget build(BuildContext context) {
@@ -135,7 +42,7 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
             // Form Body
             Expanded(
               child: SingleChildScrollView(
-                controller: _scrollController,
+                controller: c.scrollController.value,
                 child: Padding(
                   padding: screenPadding,
                   child: buildForm(),
@@ -145,19 +52,16 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
 
             const HorizontalRule(),
             FormFooter(
-              formKey: _formKey, 
-              btnIsLoading: isSubmittingForm,
+              formKey: c.formKey.value,
+              btnIsLoading: c.isSubmittingForm.value,
               press: () async {
-                setState(() { isSubmittingForm = true; });
+                c.isSubmittingForm = true.obs;
                 // submit form if form data is valid
-                print('1');
-                if (formDataIsValid()) {
-                  print('2');
+                if (c.formDataIsValid()) {
                   // submit form
-                  await submitForm();
+                  await c.submitForm();
                 }
-                print('3');
-                setState(() { isSubmittingForm = false; });
+                c.isSubmittingForm = false.obs;
               }
             ),
           ],
@@ -170,60 +74,74 @@ class _CreateClassScreenState extends State<CreateClassScreen> {
 
   Form buildForm() {
     return Form(
-      key: _formKey,
+      key: c.formKey.value,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           const SizedBox(height: 30),
 
           // Class Avatar / Cover
-          FormAddCover(press: updateClassCoverImage),
-          _coverImgFileHasError ? const TextInputError(text: 'A class requires an image cover') : const SizedBox(),
+          FormAddCover(press: (file) {
+            c.coverImgFile = file.obs;
+          },),
+          c.coverImgFileHasError.value ? const TextInputError(text: 'A class requires an image cover',) : const SizedBox(),
           
           // Class Name
           const SizedBox(height: 30),
           FormInputField(
-            controller: _titleController,
+            controller: c.titleController.value,
             hintText: 'Class Name', 
             fontSize: 20, 
             fontWeight: FontWeight.w500,
             validator: validateRequireField, 
             press: (_) {},
           ),
-          _hodHasError ? const TextInputError() : const SizedBox(),
+          c.hodHasError.value ? const TextInputError() : const SizedBox(),
 
   
           // Add Faculty
           const SizedBox(height: 15),
-          FormSelectFaculty(buttonText: 'Faculty', selectedFacultyList: _selectedFaculties, facultiesToSelectFrom: _facultiesToSelectFrom, updateSelectedFaculty: updateSelectedFaculty),
-          _hodHasError ? const TextInputError() : const SizedBox(),
+          FormSelectFaculty(
+            buttonText: 'Faculty', 
+            facultiesToSelectFrom: c.facultiesToSelectFrom.value, 
+            selectedFacultyList: c.selectedFaculties.value, 
+            updateSelectedFaculty: c.updateSelectedFaculty, 
+          ),
+
+          c.hodHasError.value ? const TextInputError() : const SizedBox(),
           const SizedBox(height: 15),
 
           const HorizontalRule(),
 
           // Description
           const SizedBox(height: 8),
-          FormDescription(controller: _descriptionController, press: (_) {}),
+          FormDescription(controller: c.descriptionController.value, press: (_) {}),
           const SizedBox(height: 5),
 
           const HorizontalRule(),
 
           // Add Educators
           const SizedBox(height: 15),
-          FormSelectUsers(buttonText: 'Educators', avatarText: 'ED', selectedUsersList: _selectedEducatorsList, usersToSelectFrom: _usersToSelectFrom, updateSelectedUsers: updateSelectedEducators,),
-          _educatorsListHasError ? const TextInputError() : const SizedBox(),
+          FormSelectUsers(
+            buttonText: 'Educators', 
+            avatarText: 'ED', 
+            selectedUsers: c.selectedEducatorsList, 
+            usersToSelectFrom: c.usersToSelectFrom, 
+            updateSelectedUsers: c.updateSelectedEducators, 
+          ),
+          c.educatorsListHasError.value ? const TextInputError() : const SizedBox(),
           const SizedBox(height: 15),
 
           const HorizontalRule(),
 
-          // Add Modules
+          // Form Modules
           const SizedBox(height: 15),
-          _modulesHasError 
+          c.modulesHasError.value 
           ? Padding(
             padding: const EdgeInsets.only(bottom: 20),
-            child: TextInputError(text: _modulesErrorMessage),
+            child: TextInputError(text: c.modulesErrorMessage.value),
           ) : const SizedBox(),
-          FormModules(modules: _modules, scrollController: _scrollController),
+          FormModules(),
 
         ],
       )
