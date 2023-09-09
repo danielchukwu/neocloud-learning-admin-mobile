943934be9fbe1e65027aed404be92fb16443a06a
dc <00danzy@gmail.com>
2023-09-08 21:12:07 -1100

    improve code

--- lib/screens/create/components/form_modules.dart
+++ lib/screens/create/components/form_modules.dart
@@ -1,37 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:neocloud_mobile/components/texts.dart';
 import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
 import 'package:neocloud_mobile/screens/create/components/form_inputfield_and_addbutton.dart';
 import 'package:neocloud_mobile/screens/create/components/form_module_tile.dart';
+import 'package:neocloud_mobile/screens/create/controllers/create_class_controller.dart';
 import 'package:neocloud_mobile/utils/utils.dart';
 
 /// This widget displays the modules (or curriculum) of a class, it is also used
 /// to further add more modules,add schedules for those modules and delete modules
-class FormModules extends StatefulWidget {
-  const FormModules(
-      {super.key, required this.modules, required this.scrollController});
+class FormModules extends StatelessWidget {
+  FormModules({super.key});
+  var c = Get.put(ClassGetXController());
 
-  final List<ClassModuleModel> modules;
-  final ScrollController scrollController;
-
-  @override
-  State<FormModules> createState() => _FormModulesState();
-}
-
-class _FormModulesState extends State<FormModules> {
-  late List<ClassModuleModel> _modules;
-
-  @override
-  void initState() {
-    super.initState();
-    _modules = widget.modules;
+  void addModule(String value) {
+    // Add module and then scroll to the bottom
+    c.addModule(value);
+    scrollToBottom(scrollController: c.scrollController.value);
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(bottom: defaultSize * 20),
+      padding: const EdgeInsets.only(bottom: 200),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -39,43 +30,21 @@ class _FormModulesState extends State<FormModules> {
           buildHeader(),
 
           // Column - List of class modules (curriculum)
-          Column(
+          Obx(() => Column(
             children: List.generate(
-              _modules.length,
+              c.modules.length,
               (index) => FormModuleTile(
-                module: _modules[index],
+                module: c.modules[index],
                 index: index,
-                pressDeleteModule: deleteModule,
-                pressUpdateModule: updateModule,
               ),
             ),
-          ),
+          )),
 
           // Input - Module InputField and Add Button
-          FormInputFieldAndAddButton(press: addModule, count: _modules.length + 1),
+          FormInputFieldAndAddButton(press: addModule),
         ],
       ),
     );
-  }
-
-  updateModule(int index, ClassModuleModel newModule) {
-    setState(() { _modules[index] = newModule; });
-  }
-
-  deleteModule(index) {
-    setState(() { _modules.removeAt(index); });
-  }
-
-  void addModule(String value) {
-    scrollToBottom(scrollController: widget.scrollController);
-
-    if (value.isNotEmpty) {
-      var newModule = ClassModuleModel(
-          id: '${_modules.length + 1}', title: value, classSchedules: []);
-      setState(() {
-        _modules.add(newModule);
-      });
-    }
   }
 
   Widget buildHeader() {
@@ -85,8 +54,8 @@ class _FormModulesState extends State<FormModules> {
       fontWeight: FontWeight.w500,
       icon: Icons.schema_outlined,
       iconColor: kBlack60,
-      iconSize: defaultSize * 1.8,
-      spaceBetweenSize: defaultSize * 1,
+      iconSize: 18,
+      spaceBetweenSize: 10,
     );
   }
 }
