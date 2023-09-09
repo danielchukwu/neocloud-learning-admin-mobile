d33f98a1d0c3ccaf1b371c315c8d53c560e357d0
dc <00danzy@gmail.com>
2023-09-08 21:17:38 -1100

    update: add DateSelection and MyTimeOfDay

--- lib/graphql/models/ClassScheduleModel.dart
+++ lib/graphql/models/ClassScheduleModel.dart
@@ -1,17 +1,46 @@
+import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
 import 'package:neocloud_mobile/graphql/models/ClassworkModel.dart';
+
+class DateSelection {
+  final int _year;
+  final int _month;
+  final int _day;
+  
+  DateSelection({required int year, required int month, required int day}) : _year = year, _month = month, _day = day;
+
+  static bool compareDateSelection(DateSelection ds1, DateSelection ds2) {
+    return ds1._year == ds2._year && ds1._month == ds2._month && ds1._day == ds2._day; 
+  }
+}
+
+class MyTimeOfDay extends TimeOfDay {
+  const MyTimeOfDay({required super.hour, required super.minute, required this.isAm});
+  
+  final bool isAm;
+
+  get convertToString => '$hour:$minute:${isAm ? 'am' : 'pm'}';
+
+  static MyTimeOfDay fromInstance({required MyTimeOfDay t, int? hour, int? minute, bool? isAm}) {
+    return MyTimeOfDay(
+      hour: hour ?? t.hour, 
+      minute: minute ?? t.minute, 
+      isAm: isAm ?? t.isAm, 
+    );
+  }
+}
+
 
 class ClassScheduleModel {
   final String id;
   final String title;
   final String? description;
-  final String? date;
+  final DateSelection? date;
   final int? order;
-  final String? startTime;
-  final String? endTime;
+  final MyTimeOfDay? startTime;
+  final MyTimeOfDay? endTime;
   final ClassworkModel? classwork;
   final ClassModuleModel? classModule;
-
 
   ClassScheduleModel({
     required this.id,
@@ -29,7 +58,8 @@ class ClassScheduleModel {
     id: cs['_id'],
     title: cs['title'],
     description: cs['description'],
-    date: cs['date'],
+    // TODO: implement date selection conversion
+    // date: cs['date'],
     order: cs['order'],
     startTime: cs['startTime'],
     endTime: cs['endTime'],
@@ -37,17 +67,29 @@ class ClassScheduleModel {
     classModule: cs.containsKey('classModule') ? ClassModuleModel.fromMap(classModule: cs['classModule']) : null,
   );
 
-  static ClassScheduleModel fromInstance({required ClassScheduleModel cs, String? title, String? description, ClassworkModel? classwork}) => ClassScheduleModel(
+  static ClassScheduleModel fromInstance({
+    required ClassScheduleModel cs, 
+    String? title, 
+    String? description, 
+    DateSelection? dateSelection,
+    ClassworkModel? classwork,
+    MyTimeOfDay? startTime,
+    MyTimeOfDay? endTime,
+  }) => ClassScheduleModel(
     id: cs.id,
     title: title ?? cs.title,
     description: description ?? cs.description,
-    date: cs.date,
+    date: dateSelection ?? cs.date,
     order: cs.order,
-    startTime: cs.startTime,
-    endTime: cs.endTime,
+    startTime: startTime ?? cs.startTime,
+    endTime: endTime ?? cs.endTime,
     classwork: classwork ?? cs.classwork,
     classModule: cs.classModule
   );
+
+  bool dateAndTimeIsPresent() {
+    return date != null && startTime != null && endTime != null;
+  }
 }
 
 // query Query {
