7aec01be287209b116de9e49ea41113ef4e9ccee
dc <00danzy@gmail.com>
2023-09-08 21:14:23 -1100

    improve code

--- lib/screens/create/components/form_description.dart
+++ lib/screens/create/components/form_description.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:neocloud_mobile/components/input/input_fields.dart';
-import 'package:neocloud_mobile/constraints.dart';
-import 'package:neocloud_mobile/size_config.dart';
 import 'package:neocloud_mobile/utils/validation.dart';
 
 class FormDescription extends StatelessWidget {
@@ -22,22 +20,21 @@ class FormDescription extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         // Icon
-        Padding(
-          padding: EdgeInsets.only(top: defaultSize * 1.2),
+        const Padding(
+          padding: EdgeInsets.only(top: 12),
           child: Icon(Icons.short_text, color: Colors.black54),
         ),
       
          // TextArea
-        SizedBox(width: defaultSize),
-        Container(
-          width: SizeConfig.screenWidth! / 1.3,
+        const SizedBox(width: 10),
+        Expanded(
           child: FormTextarea(
             controller: controller,
             maxLines: maxLine ?? 10,
             hintText: 'Add Description',
             validator: validateRequireField,
             press: press,
-          )
+          ),
         ),
       ]
     );
