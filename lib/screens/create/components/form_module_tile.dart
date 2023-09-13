2bc5e6db2f6d09408118d52f240f5b2fab65c8bd
dc <00danzy@gmail.com>
2023-09-13 23:31:48 +0100

    improve code

--- lib/screens/create/components/form_module_tile.dart
+++ lib/screens/create/components/form_module_tile.dart
@@ -7,7 +7,6 @@ import 'package:neocloud_mobile/graphql/models/ClassModuleModel.dart';
 import 'package:neocloud_mobile/screens/create/components/form_update_or_delete_inputfield.dart';
 import 'package:neocloud_mobile/screens/create/controllers/create_class_controller.dart';
 
-
 // This is a form class module tile widget that shows a modules title and it's order count
 // and then it shows the number of schedules the module has and also an add schedules button
 class FormModuleTile extends StatefulWidget {
@@ -27,6 +26,13 @@ class FormModuleTile extends StatefulWidget {
 class _FormModuleTileState extends State<FormModuleTile> {
   var c = Get.put(ClassGetXController());
   bool _editMode = false;
+  late ClassModuleModel _module;
+
+  @override
+  void initState() {
+    super.initState();
+    _module = widget.module;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +44,11 @@ class _FormModuleTileState extends State<FormModuleTile> {
           // Module Index
           const SizedBox(width: 10),
           SizedBox(
-            width: 30, 
+            width: 30,
             child: _editMode == false
-            ? TextMedium(title: '${widget.index + 1}', color: Colors.black54)
-            : const SizedBox(),
+                ? TextMedium(
+                    title: '${widget.index + 1}', color: Colors.black54)
+                : const SizedBox(),
           ),
 
           // Column - Title, Row (schedules count, add schedules btn)
@@ -53,23 +60,30 @@ class _FormModuleTileState extends State<FormModuleTile> {
 
   Widget buildBody() {
     return GestureDetector(
-      onTap: () => setState(() { _editMode = true; }),
+      onTap: () {
+        if (c.enableUpdateTitle.value == true) {
+          setState(() => _editMode = true);
+        }
+      },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // title
-          _editMode == false 
-          ? TextMedium(  title: widget.module.title, color: kBlack80, weight: FontWeight.w500)
-          : FormUpdateOrDeleteInputField(
-              fontSize: 16, 
-              textColor: kBlack80, 
-              fontWeight: FontWeight.w500, 
-              hintText: 'Schedule Title', 
-              initialValue: widget.module.title, 
-              pressUpdate: updateModule, 
-              pressDelete: () => c.deleteModule(widget.index),
-            ),
-    
+          _editMode == false
+              ? TextMedium(
+                  title: widget.module.title,
+                  color: kBlack80,
+                  weight: FontWeight.w500)
+              : FormUpdateOrDeleteInputField(
+                  fontSize: 16,
+                  textColor: kBlack80,
+                  fontWeight: FontWeight.w500,
+                  hintText: 'Schedule Title',
+                  initialValue: widget.module.title,
+                  pressUpdate: updateModule,
+                  pressDelete: deleteModule,
+                ),
+
           // row - schedules count, view schedules
           const SizedBox(height: 8),
           Row(
@@ -80,10 +94,16 @@ class _FormModuleTileState extends State<FormModuleTile> {
                 title: '${widget.module.classSchedules?.length ?? 0} Schedules',
                 color: Colors.black54,
               ),
-    
+
               // view schedules
               GestureDetector(
-                onTap: () => showCreateScheduleDialog(module: widget.module, moduleCount: widget.index, updateModule: c.updateModule),
+                onTap: () {
+                  showCreateScheduleDialog(
+                    module: widget.module,
+                    moduleCount: widget.index,
+                    updateModule: c.updateModule,
+                  );
+                },
                 child: const TextSmall(
                   title: 'View Schedules',
                   color: Colors.black54,
@@ -96,10 +116,20 @@ class _FormModuleTileState extends State<FormModuleTile> {
     );
   }
 
+  deleteSchedule(int index) {
+    setState(() => widget.module.classSchedules!.removeAt(index));
+  }
+
+  deleteModule() {
+    setState(() => _editMode = false);
+    c.deleteModule(widget.index);
+  }
+
   updateModule(String title) {
-    setState(() => _editMode = false );
+    setState(() => _editMode = false);
     if (widget.module.title != title) {
-      var newModule = ClassModuleModel.fromInstance(title: title, module: widget.module);
+      var newModule =
+          ClassModuleModel.fromInstance(title: title, module: widget.module);
       c.updateModule(widget.index, newModule);
     }
   }
