c24437605225fca4fc2b01fb300bb053b49e48cb
dc <00danzy@gmail.com>
2023-09-29 22:59:53 +0100

    first commit

---
+++ lib/features/search/presentation/bloc/search_navbar/search_navbar_event.dart
@@ -0,0 +1,11 @@
+part of 'search_navbar_bloc.dart';
+
+abstract class SearchNavbarEvent {}
+
+class SearchNavbarAllEvent extends SearchNavbarEvent {}
+
+class SearchNavbarEducatorsEvent extends SearchNavbarEvent {}
+
+class SearchNavbarStudentsEvent extends SearchNavbarEvent {}
+
+class SearchNavbarClassesEvent extends SearchNavbarEvent {}
