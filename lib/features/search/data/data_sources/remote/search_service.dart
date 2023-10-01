011bd42d52b97fc2d6e748e4c2c37021c68448e8
dc <00danzy@gmail.com>
2023-10-01 22:10:03 +0100

    complete code

--- lib/features/search/data/data_sources/remote/search_service.dart
+++ lib/features/search/data/data_sources/remote/search_service.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:neocloud_mobile/app_secure_storage.dart';
 import 'package:neocloud_mobile/components/popups/popups.dart';
+import 'package:neocloud_mobile/core/models/class_model.dart';
+import 'package:neocloud_mobile/core/models/user_model.dart';
 import 'package:neocloud_mobile/graphql/graphql_config.dart';
-import 'package:neocloud_mobile/graphql/models/ClassModel.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:neocloud_mobile/graphql/models/UserModel.dart';
 import 'package:neocloud_mobile/utils/error_handler.dart';
 
 
@@ -12,10 +11,10 @@ class SearchService {
   static var config = GraphQLConfig();
   var client = config.client;
 
-  Future<List<UserModel>> getUsers({int? limit, String? name}) async {
+  Future<List<UserModel>> getUsers({int? page, String? name}) async {
     String usersQuery = """
-      query Query(\$limit: Int, \$userName: String) {
-        users(name: \$userName, limit: \$limit) {
+      query Query(\$page: Int, \$userName: String) {
+        users(name: \$userName, page: \$page) {
           _id
           name
           bio
@@ -32,26 +31,17 @@ class SearchService {
     """;
 
     try {
-      print('TOKEN BEFORE');
-      print(await AppSecureStorage.getToken());
-      // print(await client.value);
-
+      print(page);
       var result = await client.value.query(QueryOptions(
         document: gql(usersQuery),
-        variables: {'limit': limit, 'userName': name},
+        variables: {'page': page ?? 1, 'userName': name},
       ));
-
-      print('TOKEN AFTER');
-      print(await AppSecureStorage.getToken());
-
-      print('RESULT:');
-      print(result);
 
       if (result.hasException) {
         debugPrint("getUsers Status: ❌❌");
         String error = await handleErrors(result);
         if (error == 'jwt expired') {
-          return await getUsers(limit: limit, name: name);
+          return await getUsers(page: page, name: name);
         }
         return [];
         
@@ -65,21 +55,18 @@ class SearchService {
         List<UserModel> userList =
             users!.map((user) => UserModel.fromMap(user: user)).toList();
 
-        print(userList);
-
         return userList;
       }
     } catch (e) {
       debugPrint("getUsers Status: ❌");
-      showTopAlertDialog(text: 'Something went wrong!');
       throw Exception(e);
     }
   }
 
-  Future<List<ClassModel>> getClasses({int? limit, String? name}) async {
+  Future<List<ClassModel>> getClasses({int? page, String? name}) async {
     String classesQuery = """
-      query Query(\$limit: Int, \$className: String) {
-        classes(limit: \$limit, name: \$className) {
+      query Query(\$page: Int, \$className: String) {
+        classes(page: \$page, name: \$className) {
           _id
           name
           about
@@ -103,14 +90,14 @@ class SearchService {
     try {
       var result = await client.value.query(QueryOptions(
         document: gql(classesQuery),
-        variables: {'limit': limit, 'className': name},
+        variables: {'page': page ?? 1, 'className': name},
       ));
 
       if (result.hasException) {
         debugPrint("getClasses Status: ❌❌");
         String error = await handleErrors(result);
         if (error == 'jwt expired') {
-          return await getClasses(limit: limit, name: name);
+          return await getClasses(page: page, name: name);
         }
         return [];
         
@@ -122,7 +109,7 @@ class SearchService {
         if (classes == null) return [];
 
         List<ClassModel> classList =
-          classes.map((clas) => ClassModel.fromMap(aClass: clas)).toList();
+          classes.map((clas) => ClassModel.fromMap(data: clas)).toList();
 
         return classList;
       }
