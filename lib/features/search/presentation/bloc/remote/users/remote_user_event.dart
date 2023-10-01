4c22c94b0cdcc07f2b66a61dae462b7308d67661
dc <00danzy@gmail.com>
2023-10-01 22:06:18 +0100

    improve code

--- lib/features/search/presentation/bloc/remote/users/remote_user_event.dart
+++ lib/features/search/presentation/bloc/remote/users/remote_user_event.dart
@@ -1,8 +1,12 @@
+import 'package:neocloud_mobile/core/entities/user_entity.dart';
+
 abstract class RemoteUserEvent {
   final String text;
-  const RemoteUserEvent(this.text);
+  final List<UserEntity> existingUsers;
+  final int page;
+  const RemoteUserEvent({required this.text, required this.existingUsers, required this.page});
 }
 
 class GetUserEvent extends RemoteUserEvent {
-  const GetUserEvent(super.text);
+  const GetUserEvent({required super.text, required super.existingUsers, required super.page});
 }
