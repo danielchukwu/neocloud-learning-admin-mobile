a149b31beeb84eb85764a3bf89a69e5546b52aef
dc <00danzy@gmail.com>
2023-10-01 22:09:20 +0100

    complete code

--- lib/features/search/domain/repository/class_repository.dart
+++ lib/features/search/domain/repository/class_repository.dart
@@ -1,6 +1,6 @@
+import 'package:neocloud_mobile/core/entities/class_entity.dart';
 import 'package:neocloud_mobile/core/resources/data_state.dart';
-import 'package:neocloud_mobile/features/search/domain/entities/class_entity.dart';
 
 abstract class ClassRepository {
-  Future<DataState<List<ClassEntity>>> getClasses();
+  Future<DataState<List<ClassEntity>>> getClasses({required String name, int ? page});
 }
