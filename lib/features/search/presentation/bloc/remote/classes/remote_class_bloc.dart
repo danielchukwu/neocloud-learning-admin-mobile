d187aa18d7c2732b0aa9c56f8b08b8011d07af56
dc <00danzy@gmail.com>
2023-10-01 22:07:12 +0100

    complete onGetClassesEvent Implementation

--- lib/features/search/presentation/bloc/remote/classes/remote_class_bloc.dart
+++ lib/features/search/presentation/bloc/remote/classes/remote_class_bloc.dart
@@ -14,10 +14,24 @@ class RemoteClassBloc extends Bloc<RemoteClassesEvent, RemoteClassesState> {
     on<GetClassesEvent>(onGetClassesEvent);
   }
 
-  Future<void> onGetClassesEvent(RemoteClassesEvent event, Emitter<RemoteClassesState> emit) async {
-    emit(const RemoteClassesLoadingState());
-    final dataState = await _getClassUseCase.call(param1: event.text);
-    final newState = RemoteClassesDoneState(dataState.data!);
-    emit(newState);
+  Future<void> onGetClassesEvent(RemoteClassesEvent getClassesEvent, Emitter<RemoteClassesState> emit) async {
+    if (getClassesEvent.text.isEmpty) {
+      var initialState = const RemoteClassesInitialState();
+      return emit(initialState);
+    }
+
+    if (getClassesEvent.page == 1) {
+      emit(const RemoteClassesLoadingState());
+    } else {
+      emit(RemoteClassesLoadingMoreState(getClassesEvent.existingClasses));
+    }
+
+    final dataState = await _getClassUseCase.call(param1: getClassesEvent.text, param2: getClassesEvent.page);
+    if (dataState.data != null) {
+      final newState = RemoteClassesDoneState([...getClassesEvent.existingClasses, ...dataState.data!]);
+      emit(newState);
+    }else {
+      print('Dat State Classes Error 😄!');
+    }
   }
 }
