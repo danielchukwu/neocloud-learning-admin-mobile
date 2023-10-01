def6ca0775d3a10c299f64c4e4310bb0f5e8d1a1
dc <00danzy@gmail.com>
2023-10-01 22:09:10 +0100

    complete code

--- lib/features/search/domain/repository/user_repository.dart
+++ lib/features/search/domain/repository/user_repository.dart
@@ -1,6 +1,6 @@
+import 'package:neocloud_mobile/core/entities/user_entity.dart';
 import 'package:neocloud_mobile/core/resources/data_state.dart';
-import 'package:neocloud_mobile/features/search/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
-  Future<DataState<List<UserEntity>>> getUsers({required String name, int ? limit});
+  Future<DataState<List<UserEntity>>> getUsers({required String name, int ? page});
 }
