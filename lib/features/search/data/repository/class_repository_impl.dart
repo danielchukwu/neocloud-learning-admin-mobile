0a585c42e9406b29679d2e78e03aafc1784c04da
dc <00danzy@gmail.com>
2023-10-01 22:09:38 +0100

    complete code

--- lib/features/search/data/repository/class_repository_impl.dart
+++ lib/features/search/data/repository/class_repository_impl.dart
@@ -1,4 +1,6 @@
-import 'package:neocloud_mobile/features/search/domain/entities/class_entity.dart';
+import 'package:neocloud_mobile/components/popups/popups.dart';
+import 'package:neocloud_mobile/core/entities/class_entity.dart';
+import 'package:neocloud_mobile/core/models/class_model.dart';
 import 'package:neocloud_mobile/core/resources/data_state.dart';
 import 'package:neocloud_mobile/features/search/domain/repository/class_repository.dart';
 import 'package:neocloud_mobile/features/search/data/data_sources/remote/search_service.dart';
@@ -10,9 +12,14 @@ class ClassRepositoryImpl implements ClassRepository {
   ClassRepositoryImpl(this._apiService);
 
   @override
-  Future<DataState<List<ClassEntity>>> getClasses() {
-    // TODO: implement getClasses
-    throw UnimplementedError();
+  Future<DataState<List<ClassEntity>>> getClasses({required String name, int ? page}) async {
+    try {
+      final classes = await _apiService.getClasses(page: page, name: name);
+      return DataSuccess<List<ClassModel>>(classes);
+    } on Exception catch (e) {
+      showTopAlertDialog(text: 'Something went wrong!');
+      return DataError(e);
+    }
   }
 
 }
