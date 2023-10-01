702f192ef67db07a9564872a4acae6910b66891f
dc <00danzy@gmail.com>
2023-10-01 22:07:25 +0100

    complete onGetUsers Implementation

--- lib/features/search/presentation/bloc/remote/users/remote_user_bloc.dart
+++ lib/features/search/presentation/bloc/remote/users/remote_user_bloc.dart
@@ -1,17 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:neocloud_mobile/core/entities/user_entity.dart';
 import 'package:neocloud_mobile/features/search/domain/usecases/get_users.dart';
 import 'package:neocloud_mobile/features/search/presentation/bloc/remote/users/remote_user_event.dart';
 import 'package:neocloud_mobile/features/search/presentation/bloc/remote/users/remote_user_state.dart';
-
-class RemoteUserBloc extends Bloc<RemoteUserEvent, RemoteUserState> {
+class RemoteUserBloc extends Bloc<RemoteUserEvent, RemoteUsersState> {
   final GetUsersUseCase _getUsersUseCase;
 
-  RemoteUserBloc(this._getUsersUseCase) : super(const RemoteUserWaitingState()) {
+  RemoteUserBloc(this._getUsersUseCase) : super(const RemoteUsersInitialState()) {
     on<GetUserEvent>(onGetUsers);
   }
+  Future<void> onGetUsers(GetUserEvent getUserEvent, Emitter<RemoteUsersState> emit) async {
+    if (getUserEvent.text.isEmpty) {
+      var initialState = const RemoteUsersInitialState();
+      return emit(initialState);
+    }
 
-  void onGetUsers(GetUserEvent getUserEvent, Emitter<RemoteUserState> emit) {
-    print('onGetUsers RAN!');
-    final data = _getUsersUseCase.call(param: getUserEvent.text);
+    if (getUserEvent.page == 1) {
+      emit(const RemoteUsersLoadingState());
+    } else {
+      emit(RemoteUsersLoadingMoreState(getUserEvent.existingUsers));
+    }
+    
+    final dataState = await _getUsersUseCase.call(param1: getUserEvent.text, param2: getUserEvent.page);
+
+    if (dataState.data != null) {
+      final newState = RemoteUsersDoneState([...getUserEvent.existingUsers, ...dataState.data!]);
+      emit(newState);
+    } else {
+      print('Bloc Data State Error Encountered 😄!');
+    }
   }
 }
