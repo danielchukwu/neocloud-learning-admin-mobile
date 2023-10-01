deda96c8db3661078978704b6d588a6f9a5ed79a
dc <00danzy@gmail.com>
2023-10-01 22:09:02 +0100

    complete code

--- lib/features/search/domain/usecases/get_class.dart
+++ lib/features/search/domain/usecases/get_class.dart
@@ -1,15 +1,15 @@
+import 'package:neocloud_mobile/core/entities/class_entity.dart';
 import 'package:neocloud_mobile/core/resources/data_state.dart';
-import 'package:neocloud_mobile/features/search/domain/entities/class_entity.dart';
 import 'package:neocloud_mobile/features/search/domain/repository/class_repository.dart';
 import 'package:neocloud_mobile/features/search/domain/usecases/usecase.dart';
 
-class GetClassUseCase implements UseCase {
+class GetClassUseCase implements UseCase<DataState<List<ClassEntity>>, String, int> {
   final ClassRepository _classRepository;
 
   const GetClassUseCase(this._classRepository);
   
   @override
-  Future<DataState<List<ClassEntity>>> call({param}) {
-    return _classRepository.getClasses();
+  Future<DataState<List<ClassEntity>>> call({required String param1, int ? param2}) {
+    return _classRepository.getClasses(name: param1, page: param2);
   }
 }
